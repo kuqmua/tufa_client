00dc73daf5579ca1e90c4f3b977df2b5d075d41d
kuqmua <serezhashmakov@outlook.com>
2022-04-20 22:52:42 +0300

    mods restructure

---
+++ src/entry.rs
@@ -0,0 +1,6 @@
+use crate::model::Model;
+
+pub fn entry() {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Model>();
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub mod form;
+pub mod helpers;
+pub mod input_button;
+pub mod svg_icon_wrapper;
+pub mod model;
+pub mod entry;
