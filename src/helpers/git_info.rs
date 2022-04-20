829f171c1bb80d6fd177af934c733b90082d051e
kuqmua <serezhashmakov@outlook.com>
2022-04-20 22:13:29 +0300

    lazy_static = "1.4"

---
+++ src/helpers/git_info.rs
@@ -0,0 +1,6 @@
+use tufa_common::helpers::git::git_info::GitInformation;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref GIT_INFO: GitInformation = GitInformation::get_git_commit_info("./");
+}
