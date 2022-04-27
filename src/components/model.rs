c36e9925f3e94065458cd0a794f24f108df4020d
kuqmua <serezhashmakov@outlook.com>
2022-04-27 21:40:42 +0300

    route example

--- src/components/model.rs
+++ src/components/model.rs
@@ -3,6 +3,52 @@ use crate::components::input_button::InputButton;
 use crate::components::svg_icon_wrapper::SvgIconWrapper;
 use yew::prelude::*;
 
+// 
+use yew_router::prelude::*;
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/secure")]
+    Secure,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+#[function_component(Secure)]
+fn secure() -> Html {
+    let history = use_history().unwrap();
+
+    let onclick = Callback::once(move |_| history.push(Route::Home));
+    html! {
+        <div>
+            <h1>{ "Secure" }</h1>
+            <button {onclick}>{ "Go Home" }</button>
+        </div>
+    }
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! { <h1>{ "Home" }</h1> },
+        Route::Secure => html! {
+            <Secure />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+#[function_component(Main)]
+fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
+    }
+}
+//
 pub enum ModalMessage {
     ActionOne,
 }
