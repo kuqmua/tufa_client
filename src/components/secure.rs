421da43ac678cd1736af61ace26907976fdafa07
kuqmua <serezhashmakov@outlook.com>
2022-04-30 14:17:04 +0300

    if let example usage

--- src/components/secure.rs
+++ src/components/secure.rs
@@ -9,14 +9,18 @@ pub fn secure() -> Html {
 
     let onclick = Callback::once(move |_| history.push(Routes::Home));
     let class = "my_title";
+    let option = Some("kekw");
     html! {
         <div>
-            if class != "my_title" {
-                <h1 class={class}>{ "Secure" }</h1>
+            if class == "my_title" {
+                <h1 class={class}>{ "my_title" }</h1>
             }
             else {
-                <h1 class={class}>{ "kekw" }</h1>
+                <h1 class={class}>{ "not my_title" }</h1>
             }
+            if let Some(option) = option {
+                <h1 class={class}>{ "some kekw" }</h1>
+            } 
             <button {onclick}>{ "Go Home" }</button>
         </div>
     }
