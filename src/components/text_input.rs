abcb807418b00459b49ea7f5bd7268cfe99ca3d4
kuqmua <serezhashmakov@outlook.com>
2022-05-02 19:18:30 +0300

    event.target()

--- src/components/text_input.rs
+++ src/components/text_input.rs
@@ -1,3 +1,4 @@
+use gloo::console::log;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq)]
@@ -7,7 +8,11 @@ pub struct TextInputProps {
 
 #[function_component(TextInput)]
 pub fn text_input(props: &TextInputProps) -> Html {
+    let onchange = Callback::from(|event: Event| {
+        let target = event.target().unwrap();
+        log!(target);
+    });
     html! {
-        <input type="text" name={props.name.clone()}/>
+        <input type="text" name={props.name.clone()} onchange={onchange}/>
     }
 }
