3fb3e029de6f5d5592248432c601f2c6a42bd173
kuqmua <serezhashmakov@outlook.com>
2022-05-02 11:57:27 +0300

    using enum as prop

--- src/components/secure.rs
+++ src/components/secure.rs
@@ -1,27 +1,48 @@
-use yew::{html, Callback, function_component, Html, Properties};
-use yew_router::hooks::use_history;
 use crate::routes::routes::Routes;
-use yew_router::prelude::*;
 use stylist::yew::styled_component;
 use stylist::{style, Style};
+use yew::{html, Callback, Html, Properties};
+use yew_router::hooks::use_history;
 use yew_router::prelude::*;
 
 #[derive(Properties, PartialEq)]
 pub struct SecureProps {
-    pub first: String
+    pub first: String,
+    pub color: Color,
 }
 
-const STYLE_FILE: &'static str = include_str!("example.css");
+const STYLE_FILE: &str = include_str!("example.css");
+
+#[derive(PartialEq)]
+pub enum Color {
+    Normal,
+    Ok,
+    Error,
+}
+
+impl Color {
+    pub fn to_string(&self) -> String {
+        match &self {
+            Color::Normal => "normal".to_string(),
+            Color::Ok => "ok".to_string(),
+            Color::Error => "error".to_string(),
+        }
+    }
+}
 
 #[styled_component(Secure)]
-pub fn secure(props: & SecureProps) -> Html {
+pub fn secure(props: &SecureProps) -> Html {
     let history = use_history().unwrap();
 
     let onclick = Callback::once(move |_| history.push(Routes::Home));
     let class = "my_title";
     let option: Option<&str> = Some("kekw");
     let example_list = vec!["one", "two", "three"];
-    let example_list_html = vec![html!{ <div>{"one"}</div>}, html!{ <div>{"two"}</div>}, html!{ <div>{"three"}</div>}];
+    let example_list_html = vec![
+        html! { <div>{"one"}</div>},
+        html! { <div>{"two"}</div>},
+        html! { <div>{"three"}</div>},
+    ];
     let example_list_for_ter = vec!["oneiter", "twoiter", "threeiter"];
     let example_list_for_function = vec!["one_function", "two_function", "three_function"];
 
@@ -29,7 +50,8 @@ pub fn secure(props: & SecureProps) -> Html {
         r#"
             background-color: blue;
         "#
-    ).unwrap();
+    )
+    .unwrap();
     let file_stylesheet = Style::new(STYLE_FILE).unwrap();
     //<h1 class={file_stylesheet}>{"file_stylesheet"}</h1>
     html! {
@@ -44,12 +66,13 @@ pub fn secure(props: & SecureProps) -> Html {
             }
             if let Some(option) = option {
                 <h1 class={class}>{option}</h1>
-            } 
+            }
             else  {
                 <h1 class={class}>{"None"}</h1>
             }
             <h1 class={css!("color: red; background-color: green;")}>{"css! macro"}</h1>
             <h1>{&props.first}</h1>
+            <h1>{&props.color.to_string()}</h1>
             {example_list}
             {example_list_html}
             {example_list_for_ter.iter().map(|x| html!{<li>{"iter of"}{x}</li>}).collect::<Html>()}
@@ -60,5 +83,5 @@ pub fn secure(props: & SecureProps) -> Html {
 }
 
 pub fn list_to_html(vec: Vec<&str>) -> Vec<Html> {
-    vec.iter().map(|x| html!{<li>{x}</li>}).collect()
+    vec.iter().map(|x| html! {<li>{x}</li>}).collect()
 }
--- src/routes/switch.rs
+++ src/routes/switch.rs
@@ -1,16 +1,15 @@
-use yew::{Html, html};
-use crate::components::auth_modal::AuthModal;
+use crate::components::secure::Secure;
+use crate::components::{auth_modal::AuthModal, secure::Color};
 use crate::routes::routes::Routes;
-use crate::components::secure::{Secure};
+use yew::{html, Html};
 
 pub fn switch(routes: &Routes) -> Html {
     match routes {
-        Routes::Home => html!
-        {
-         <AuthModal/> 
+        Routes::Home => html! {
+         <AuthModal/>
         },
         Routes::Secure => html! {
-            <Secure first="my_first_prop"/>
+            <Secure first="my_first_prop" color={Color::Ok}/>
         },
         Routes::NotFound => html! { <h1>{ "404" }</h1> },
     }
