69bd3d8f4af23a4e82e3dc0764a4d2868ada36a6
kuqmua <serezhashmakov@outlook.com>
2022-05-09 13:52:37 +0300

    cleaning

--- src/components/app.rs
+++ src/components/app.rs
@@ -7,20 +7,9 @@ use crate::routes::routes::Routes;
 use crate::routes::switch::switch;
 use crate::store::init;
 use crate::store::YewduxStore;
-// use wasm_bindgen::JsCast;
-// use web_sys::HtmlInputElement;
-use gloo::console::log;
-use lazy_static::__Deref;
-use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_router::prelude::*;
 use yewdux::prelude::*;
-
-// use gloo::console::log;
-use reqwasm::http::Request;
-use serde_json::from_str;
-use tufa_common::json_example::JsonExample;
-use wasm_bindgen::JsCast;
 
 pub enum AppMessage {
     ActionOne,
@@ -40,115 +29,6 @@ impl Component for App {
         true
     }
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // let handle_form_submit = Callback::from(|event: FocusEvent| {
-        //     event.prevent_default();
-        // });
-        // let handle_username_change =
-        //     ctx.props()
-        //         .dispatch()
-        //         .reduce_callback_with(|state, event: Event| {
-        //             let username = event
-        //                 .target()
-        //                 .unwrap()
-        //                 .unchecked_into::<HtmlInputElement>()
-        //                 .value();
-        //             state.username = username;
-        //         });
-        // let onclick = Callback::from(move |_| {
-        //     wasm_bindgen_futures::spawn_local(async move {
-        //         log!("Update1");
-        //         let f = Request::get("http://127.0.0.1:8081/api/json/json_example")
-        //             .send()
-        //             .await;
-        //         match f {
-        //             Ok(k) => {
-        //                 match k.text().await {
-        //                     Ok(n) => {
-        //                         log!(format!("ok {:#?}", n));
-        //                         let json: Result<JsonExample, serde_json::Error> = from_str(&n);
-        //                         match json {
-        //                             Ok(_l) => {
-        //                                 // let mut bbb = l.second.clone();
-        //                                 // self.some_string = "ertrer".to_string();
-        //                                 // self.set_second(bbb);
-        //                                 // log!("ok {:#?}", l);
-        //                             }
-        //                             Err(e) => log!(format!("2err {:#?}", e)),
-        //                         }
-        //                     }
-        //                     Err(e) => log!(format!("1err {:#?}", e)),
-        //                 }
-        //                 log!("ok {:#?}", k.body());
-        //             }
-        //             Err(_) => log!("0err"),
-        //         }
-        //         log!("Update2:");
-        //     });
-        // });
-        // let handle_username_change =
-        //     ctx.props()
-        //         .dispatch()
-        //         .reduce_callback_with(|state, event: Event| {
-        //             // let username = event
-        //             //     .target()
-        //             //     .unwrap()
-        //             //     .unchecked_into::<HtmlInputElement>()
-        //             //     .value();
-        //             // state.username = username;
-        //             // log!("username", state.username.clone());
-        //             // let mut nnn = String::from("yyey");
-        //             let mut f: JsonExample = JsonExample {
-        //                 first: String::from("111"),
-        //                 second: String::from("222"),
-        //             };
-        //             wasm_bindgen_futures::spawn_local(async move {
-        //                 log!("Update1");
-        //                 let b = Request::get("http://127.0.0.1:8081/api/json/json_example")
-        //                     .send()
-        //                     .await
-        //                     .unwrap()
-        //                     .json::<JsonExample>()
-        //                     .await
-        //                     .unwrap();
-        //                 let mut state_cloned = state.deref().clone();
-        //                 state_cloned.username = b.second;
-        //                 // state.set(state_cloned);
-        //                 state = &mut state_cloned;
-        //                 // f = b;
-        //                 // return b;
-        //                 // state.username =  f.second;
-        //                 // match f {
-        //                 //     Ok(k) => {
-        //                 //         match k.text().await {
-        //                 //             Ok(n) => {
-        //                 //                 log!(format!("ok {:#?}", n));
-        //                 //                 let json: Result<JsonExample, serde_json::Error> =
-        //                 //                     from_str(&n);
-        //                 //                 match json {
-        //                 //                     Ok(l) => {
-        //                 //                         // nnn = l.second.clone();
-        //                 //                         f = JsonExample {
-        //                 //                             first: l.first,
-        //                 //                             second: l.second,
-        //                 //                         };
-        //                 //                         // let mut bbb = l.second.clone();
-        //                 //                         // self.some_string = "ertrer".to_string();
-        //                 //                         // self.set_second(bbb);
-        //                 //                         // log!("ok {:#?}", l);
-        //                 //                     }
-        //                 //                     Err(e) => log!(format!("2err {:#?}", e)),
-        //                 //                 }
-        //                 //             }
-        //                 //             Err(e) => log!(format!("1err {:#?}", e)),
-        //                 //         }
-        //                 //         log!("ok {:#?}", k.body());
-        //                 //     }
-        //                 //     Err(_) => log!("0err"),
-        //                 // }
-        //                 // log!("Update2:");
-        //             });
-        //             state.username = f.second;
-        //         });
         html! {
           <div
               style="
@@ -159,10 +39,7 @@ impl Component for App {
                   align-items: center;
               "
           >
-
               <Header/>
-            //   <button onclick={get_message}>{"click me to fetch data from server"}</button>
-            //   <button onclick={onclick}>{"click me to fetch data from server"}</button>
               <GetDataFromServerButton/>
               <YewduxFunctionalComponentExample/>
               <WithDispatch<DisplayCount>/>
