9dfe50e79e08b86b237b35740b33ebd69cd64014
kuqmua <serezhashmakov@outlook.com>
2022-05-14 13:54:45 +0300

    #[function_component(PostsList)]

---
+++ src/components/feed/posts_list.rs
@@ -0,0 +1,19 @@
+use yew::prelude::*;
+
+#[function_component(PostsList)]
+pub fn posts_list() -> Html {
+    html! {
+      <div
+        style="
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: flex-start;
+          width: 500px;
+          background-color: #1E2832;
+        "
+      >
+
+      </div>
+    }
+}
