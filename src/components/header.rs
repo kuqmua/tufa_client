5a7d67a803021e57dd4d9818ba0120aeb9567186
kuqmua <serezhashmakov@outlook.com>
2022-05-15 14:59:31 +0300

    lil style

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,3 +1,4 @@
+use crate::components::svg::menu::Menu;
 use crate::routes::routes::Routes;
 use yew::{function_component, html};
 use yew_router::prelude::Link;
@@ -5,28 +6,41 @@ use yew_router::prelude::Link;
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
-        <header
+      <header
+        style="
+          height: 50px; 
+          width: 100%; 
+          background-color: #16202A;
+          border-bottom: 1px solid #ffffa2;
+          position: fixed;
+        ">
+          <div
             style="
-                height: 50px; 
-                width: 100%; 
-                background-color: #16202A;
-                border-bottom: 1px solid #ffffa2;
-                display: flex; 
-                justify-content: space-between; 
-                align-items: center;
-                position: fixed;
-            ">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 15h18v-2H3v2zm0 4h18v-2H3v2zm0-8h18V9H3v2zm0-6v2h18V5H3z"/></svg>
+              display: flex; 
+              justify-content: space-between; 
+              align-items: center;
+              padding-right: 8px;
+              padding-left: 8px;
+            "
+          >
+            <div
+              style="
+                padding: 8px;
+              "
+            >
+              <Menu height={"24px".to_owned()} width={"24px".to_owned()} fill={"#A2B0B9".to_owned()}/>
+            </div>
             <Link<Routes> to={Routes::Home}>{ "home" }</Link<Routes>>
             <div
               style="
                 color: white
               "
             >
-                <Link<Routes> to={Routes::SignUp}>{ "sign up" }</Link<Routes>>
+              <Link<Routes> to={Routes::SignUp}>{ "sign up" }</Link<Routes>>
                 {"----------"}
-                <Link<Routes> to={Routes::SignIn}>{ "sign ip" }</Link<Routes>>
+              <Link<Routes> to={Routes::SignIn}>{ "sign ip" }</Link<Routes>>
             </div>
+          </div>
         </header>
     }
 }
