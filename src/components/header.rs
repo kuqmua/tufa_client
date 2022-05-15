36ec1f225437f851b106c88b336b4b18a8acdf1c
kuqmua <serezhashmakov@outlook.com>
2022-05-15 14:38:08 +0300

    lil style

--- src/components/header.rs
+++ src/components/header.rs
@@ -9,7 +9,8 @@ pub fn header() -> Html {
             style="
                 height: 50px; 
                 width: 100%; 
-                background-color: yellow; 
+                background-color: #16202A;
+                border-bottom: 1px solid #ffffa2;
                 display: flex; 
                 justify-content: space-between; 
                 align-items: center;
@@ -17,7 +18,11 @@ pub fn header() -> Html {
             ">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 15h18v-2H3v2zm0 4h18v-2H3v2zm0-8h18V9H3v2zm0-6v2h18V5H3z"/></svg>
             <Link<Routes> to={Routes::Home}>{ "home" }</Link<Routes>>
-            <div>
+            <div
+              style="
+                color: white
+              "
+            >
                 <Link<Routes> to={Routes::SignUp}>{ "sign up" }</Link<Routes>>
                 {"----------"}
                 <Link<Routes> to={Routes::SignIn}>{ "sign ip" }</Link<Routes>>
