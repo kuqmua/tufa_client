3a13fbab7d31527c40b1d3dcaf8fc3e69ac7c9f7
kuqmua <serezhashmakov@outlook.com>
2022-05-17 23:01:37 +0300

    #[function_component(PersonOutlineButton)]

---
+++ src/components/header/buttons/person_outline_button.rs
@@ -0,0 +1,19 @@
+use crate::components::header::buttons::button_wrapper::ButtonWrapper;
+use crate::components::svg::person_outline::PersonOutline;
+use web_sys::MouseEvent;
+use yew::{function_component, html, Callback, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct PersonOutlineButtonProps {
+    pub callback: Callback<MouseEvent>,
+}
+
+#[function_component(PersonOutlineButton)]
+pub fn person_outline_button(props: &PersonOutlineButtonProps) -> Html {
+    let icon_size = "26px".to_owned();
+    let html_handle =
+        html! {<PersonOutline height={icon_size.clone()} width={icon_size} fill={"#5B6267".to_owned()}/>};
+    html! {
+      <ButtonWrapper inner_html={html_handle} callback={props.callback.clone()}/>
+    }
+}
