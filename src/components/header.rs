33f32dae0ec7fd071163fb112a71000fde9dc1e0
kuqmua <serezhashmakov@outlook.com>
2022-05-17 22:12:46 +0300

    ProfileActionsPanel

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,6 +1,6 @@
 use crate::components::svg::menu::Menu;
 use crate::components::svg::person_outline::PersonOutline;
-use crate::components::svg::logout::Logout;
+use crate::components::profile_actions_panel::ProfileActionsPanel;
 // use crate::routes::routes::Routes;
 use yew::{function_component, html};
 // use yew_router::prelude::Link;
@@ -67,52 +67,7 @@ pub fn header() -> Html {
               </div>
             </div>
           </div>
-          <div
-            style="
-              position: absolute;
-              top: 43px;
-              right: 0px;
-              height: 400px;
-              width: 190px;
-              border-radius: 0px 0px 0px 20px;
-              border-left: 1px solid #222c36;
-              border-bottom: 1px solid #222c36;
-              background-color: #16202A;
-              display: flex;
-              justify-content: space-evenly;
-              flex-direction: column;
-              padding: 13px;
-            "
-          >
-            <div
-              style="
-                color: white;
-                display: flex;
-                flex-direction: row;
-                padding-top: 5px;
-                padding-bottom: 5px;
-                padding-left: 10px;
-                padding-right: 10px;
-                background-color: #ffffff33;
-                border-radius: 5px 5px 5px 5px;
-                height: 25px;
-                display: flex;
-                align-items: center;
-              "
-            >
-              <Logout height={"22px".to_owned()} width={"22px".to_owned()} fill={"white".to_owned()}/>
-              <div
-                style="
-                  color: white;
-                  margin-left: 10px;
-                  display: flex;
-                  align-items: center;
-                "
-              >
-                {"Logout"}
-              </div>
-            </div>
-          </div>
+          <ProfileActionsPanel/>
       </header>
     }
 }
