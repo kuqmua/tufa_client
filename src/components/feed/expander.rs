88284ee7a1295269bc27adf4d221be0e877ecd41
kuqmua <serezhashmakov@outlook.com>
2022-05-18 22:20:56 +0300

    style_handle

--- src/components/feed/expander.rs
+++ src/components/feed/expander.rs
@@ -1,25 +1,31 @@
+use crate::constants::INTERFACE_LINES_COLOR;
 use yew::{function_component, html};
 
 #[function_component(Expander)]
 pub fn expander() -> Html {
+    let style_handle = format!(
+        "
+        height: 400px; 
+        width: 486px;
+        min-width: 470px;
+        background-color: #16202A;
+        border-top: 1px solid {};
+        border-left: 1px solid {};
+        border-right: 1px solid {};
+        position: fixed;
+        border-radius: 30px 30px 0px 0px;
+        bottom: 0px;
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+        flex-direction: column;
+      ",
+        INTERFACE_LINES_COLOR, INTERFACE_LINES_COLOR, INTERFACE_LINES_COLOR
+    );
     html! {
       <div
-        style="
-          height: 400px; 
-          width: 486px;
-          min-width: 470px;
-          background-color: #16202A;
-          border-top: 1px solid white;
-          border-left: 1px solid white;
-          border-right: 1px solid white;
-          position: fixed;
-          border-radius: 30px 30px 0px 0px;
-          bottom: 0px;
-          display: flex;
-          justify-content: space-evenly;
-          align-items: center;
-          flex-direction: column;
-        ">
+        style={style_handle}
+      >
           <div
             style="
               color: white
