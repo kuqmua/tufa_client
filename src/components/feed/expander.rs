8d8a219cd31ca9a2ab33dd067140677fd54c6b52
kuqmua <serezhashmakov@outlook.com>
2022-05-18 22:40:12 +0300

    lil style

--- src/components/feed/expander.rs
+++ src/components/feed/expander.rs
@@ -6,8 +6,8 @@ pub fn expander() -> Html {
     let style_handle = format!(
         "
         height: 400px; 
-        width: 486px;
-        min-width: 470px;
+        width: 466px;
+        min-width: 466px;
         background-color: #16202A;
         border-top: 1px solid {};
         border-left: 1px solid {};
@@ -16,9 +16,9 @@ pub fn expander() -> Html {
         border-radius: 30px 30px 0px 0px;
         bottom: 0px;
         display: flex;
-        justify-content: space-evenly;
         align-items: center;
         flex-direction: column;
+        padding: 10px;
       ",
         INTERFACE_LINES_COLOR, INTERFACE_LINES_COLOR, INTERFACE_LINES_COLOR
     );
@@ -28,21 +28,37 @@ pub fn expander() -> Html {
       >
           <div
             style="
-              color: white
-          ">
-            {"one"}
-          </div>
+              background-color: #ffffa2;
+              height: 5px;
+              width: 60px;
+              border-radius: 3px;
+          "/>
           <div
             style="
-              color: white
+              display: flex;
+              justify-content: space-evenly;
+              align-items: center;
+              flex-direction: column;
+              height: 100%;
           ">
-            {"two"}
-          </div>
-          <div
-            style="
-              color: white
-          ">
-            {"three"}
+            <div
+              style="
+                color: white
+            ">
+                {"one"}
+            </div>
+            <div
+              style="
+                color: white
+            ">
+              {"two"}
+            </div>
+            <div
+              style="
+                color: white
+            ">
+              {"three"}
+            </div>
           </div>
       </div>
     }
