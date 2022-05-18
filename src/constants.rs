abf6dd4c7e80607a47b39d331f097bd137b7df1b
kuqmua <serezhashmakov@outlook.com>
2022-05-18 22:13:13 +0300

    FEED_BUTTONS_BACKGROUND_COLOR

--- src/constants.rs
+++ src/constants.rs
@@ -2,3 +2,4 @@ pub const PROJECT_NAME: &str = "tufa_client";
 pub const HEADER_ICONS_COLOR: &str = "#5B6267";
 pub const FEED_ICONS_COLOR: &str = "#5B6267";
 pub const INTERFACE_LINES_COLOR: &str = "#222c36";
+pub const FEED_BUTTONS_BACKGROUND_COLOR: &str = "#222c36";
