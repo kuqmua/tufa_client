cba5ab3058c09811e8ee9fe3db275f9b5f9ac31e
kuqmua <serezhashmakov@outlook.com>
2022-05-18 22:16:39 +0300

    use crate::constants::FEED_BUTTONS_BACKGROUND_COLOR;

--- src/components/feed/buttons/button_wrapper.rs
+++ src/components/feed/buttons/button_wrapper.rs
@@ -1,3 +1,4 @@
+use crate::constants::FEED_BUTTONS_BACKGROUND_COLOR;
 use web_sys::MouseEvent;
 use yew::{function_component, html, Callback, Html, Properties};
 
@@ -9,19 +10,23 @@ pub struct ButtonWrapperProps {
 
 #[function_component(ButtonWrapper)]
 pub fn button_wrapper(props: &ButtonWrapperProps) -> Html {
+    let style_handle = format!(
+        "
+        border-radius: 10px;
+        border: 1px solid {};
+        width: 35px;
+        height: 35px;
+        margin-bottom: 8px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: {};
+      ",
+        FEED_BUTTONS_BACKGROUND_COLOR, FEED_BUTTONS_BACKGROUND_COLOR
+    );
     html! {
       <button
-        style="
-          border-radius: 10px;
-          border: 1px solid #222c36;
-          width: 35px;
-          height: 35px;
-          margin-bottom: 8px;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color: #222c36;
-        "
+        style={style_handle}
         onclick={&props.callback}
       >
        {props.inner_html.clone()}
