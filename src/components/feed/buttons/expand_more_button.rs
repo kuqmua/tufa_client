7e057c267a3c7f33fbd32bf40f6dbcf4fdc16bb5
kuqmua <serezhashmakov@outlook.com>
2022-05-18 21:11:09 +0300

    fix constant usage

--- src/components/feed/buttons/expand_more_button.rs
+++ src/components/feed/buttons/expand_more_button.rs
@@ -1,5 +1,6 @@
 use crate::components::feed::buttons::button_wrapper::ButtonWrapper;
 use crate::components::svg::expand_more::ExpandMore;
+use crate::constants::FEED_ICONS_COLOR;
 use web_sys::MouseEvent;
 use yew::{function_component, html, Callback, Properties};
 
@@ -11,7 +12,7 @@ pub struct ExpandMoreProps {
 #[function_component(ExpandMoreButton)]
 pub fn expand_more_button(props: &ExpandMoreProps) -> Html {
     let icon_size = "24px".to_owned();
-    let html_handle = html! {<ExpandMore height={icon_size.clone()} width={icon_size} fill={"#5B6267".to_owned()}/>};
+    let html_handle = html! {<ExpandMore height={icon_size.clone()} width={icon_size} fill={FEED_ICONS_COLOR.to_owned()}/>};
     html! {
       <ButtonWrapper inner_html={html_handle} callback={&props.callback}/>
     }
