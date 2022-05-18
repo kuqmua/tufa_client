b18b2bef7da98bd826aba2281d3726adfa829e5b
kuqmua <serezhashmakov@outlook.com>
2022-05-18 21:12:09 +0300

    MenuButton usage inside header

--- src/components/header/component.rs
+++ src/components/header/component.rs
@@ -1,7 +1,6 @@
+use crate::components::header::buttons::menu_button::MenuButton;
 use crate::components::header::buttons::person_outline_button::PersonOutlineButton;
 use crate::components::header::profile_actions_panel::ProfileActionsPanel;
-use crate::components::svg::menu::Menu;
-use crate::constants::HEADER_ICONS_COLOR;
 use web_sys::MouseEvent;
 // use crate::routes::routes::Routes;
 use yew::{function_component, html, use_state, Callback};
@@ -13,6 +12,9 @@ pub fn header() -> Html {
     let profile_actions_panel_opened_cloned = profile_actions_panel_opened.clone();
     let change_profile_actions_panel_opened: Callback<MouseEvent> = Callback::from(move |_| {
         profile_actions_panel_opened_cloned.set(!*profile_actions_panel_opened_cloned);
+    });
+    let do_something_with_menu: Callback<MouseEvent> = Callback::from(move |_| {
+        println!("this message will not be shown");
     });
     html! {
       <header
@@ -42,15 +44,7 @@ pub fn header() -> Html {
                 padding-left: 20px;
               "
             >
-              <div
-                style="
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                "
-              >
-                <Menu height={"26px".to_owned()} width={"26px".to_owned()} fill={HEADER_ICONS_COLOR.to_owned()}/>
-              </div>
+              <MenuButton callback={do_something_with_menu}/>
               <div
                 style="
                   font-size: 30px;
