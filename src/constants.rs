8d6e3142f24f26d57f0abad85d8f2ec19891ae56
kuqmua <serezhashmakov@outlook.com>
2022-05-19 20:22:52 +0300

    BACKGROUND_COLOR

--- src/constants.rs
+++ src/constants.rs
@@ -4,3 +4,4 @@ pub const FEED_ICONS_COLOR: &str = "#5B6267";
 pub const INTERFACE_LINES_COLOR: &str = "#222c36";
 pub const FEED_BUTTONS_BACKGROUND_COLOR: &str = "#222c36";
 pub const ACTIVE_COLOR: &str = "#ffffa2";
+pub const BACKGROUND_COLOR: &str = "#16202A";
