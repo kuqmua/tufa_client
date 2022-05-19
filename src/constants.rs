a6dbf55ae28a892a695169d32aeb6112513cd751
kuqmua <serezhashmakov@outlook.com>
2022-05-19 20:44:41 +0300

    FEED_WIDTH

--- src/constants.rs
+++ src/constants.rs
@@ -5,3 +5,4 @@ pub const INTERFACE_LINES_COLOR: &str = "#222c36";
 pub const FEED_BUTTONS_BACKGROUND_COLOR: &str = "#222c36";
 pub const ACTIVE_COLOR: &str = "#ffffa2";
 pub const BACKGROUND_COLOR: &str = "#16202A";
+pub const FEED_WIDTH: u32 = 470;
