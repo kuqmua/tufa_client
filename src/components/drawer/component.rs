ba5ea448b286477d5654d5e3130c1816fee2d20e
kuqmua <serezhashmakov@outlook.com>
2022-05-21 17:17:09 +0300

    restructure drawer component

--- src/components/drawer/component.rs
+++ src/components/drawer/component.rs
@@ -1,14 +1,34 @@
-use yew::{function_component, html};
+use web_sys::MouseEvent;
+use yew::{function_component, html, Properties, Callback};
+
+#[derive(Properties, PartialEq)]
+pub struct DrawerProps {
+    pub style: String,
+    pub callback: Callback<MouseEvent>,
+}
 
 #[function_component(Drawer)]
-pub fn drawer() -> Html {
+pub fn drawer(props: &DrawerProps) -> Html {
     html! {
-      <>
-      <input type="checkbox" id="menu-opener"/>
-      <aside class="DrawerMenu" id="menu">
-          <nav class="Menu"></nav>
-          <label for="menu-opener" style="background-color: black; opacity: 0.1;"></label>
+      <aside 
+        style={props.style.clone()}
+      >
+        <nav 
+          style="
+            transition: all 500ms cubic-bezier(0.4, 0.0, 0.2, 1);
+            background-color: rgb(21, 255, 0);
+          "
+        >
+        </nav>
+        <label 
+          for="menu-opener" 
+          style="
+            background-color: yellow; 
+            opacity: 0.5;
+          "
+          onclick={&props.callback}
+        >
+        </label>
       </aside>
-      </>
     }
 }
