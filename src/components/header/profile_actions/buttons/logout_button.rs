001b99b60fd006c6c4b62c1eab7b1c6d5ae074b8
kuqmua <serezhashmakov@outlook.com>
2022-05-22 17:57:03 +0300

    #[function_component(SettingsButton)]

--- src/components/header/profile_actions/buttons/logout_button.rs
+++ src/components/header/profile_actions/buttons/logout_button.rs
@@ -2,7 +2,6 @@ use crate::components::header::profile_actions::buttons::button_wrapper::ButtonW
 use crate::components::svg::logout::Logout;
 use web_sys::MouseEvent;
 use yew::{function_component, html, Callback, Properties};
-// use crate::constants::HEADER_ICONS_COLOR;
 
 #[derive(Properties, PartialEq)]
 pub struct LogoutButtonProps {
@@ -12,10 +11,9 @@ pub struct LogoutButtonProps {
 #[function_component(LogoutButton)]
 pub fn logout_button(props: &LogoutButtonProps) -> Html {
     let icon_size = "22px".to_owned();
-    let html_handle = html! {<Logout height={icon_size.clone()} width={icon_size} fill={"white".to_owned()}/>};
+    let html_handle =
+        html! {<Logout height={icon_size.clone()} width={icon_size} fill={"white".to_owned()}/>};
     html! {
       <ButtonWrapper inner_html={html_handle} callback={props.callback.clone()} placeholder={"Log Out".to_owned()}/>
     }
 }
-
-
--- src/components/header/profile_actions/buttons/mod.rs
+++ src/components/header/profile_actions/buttons/mod.rs
@@ -1,2 +1,3 @@
-pub mod logout_button;
 pub mod button_wrapper;
+pub mod logout_button;
+pub mod settings_button;
