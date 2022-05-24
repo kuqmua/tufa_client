a4715916cd8685b8c1f0ea150b674131899e018d
kuqmua <serezhashmakov@outlook.com>
2022-05-24 21:52:52 +0300

    CenterFormWrapper usage inside SignIn

--- src/components/authorization/sign_in/component.rs
+++ src/components/authorization/sign_in/component.rs
@@ -11,6 +11,7 @@ use web_sys::HtmlInputElement;
 use crate::helpers::html_input_type::HtmlInputType;
 use crate::components::authorization::input_form::InputForm;
 use yewdux_functional::use_store;
+use crate::components::authorization::center_form_wrapper::CenterFormWrapper;
 
 #[function_component(SignIn)]
 pub fn sign_in() -> Html {
@@ -52,138 +53,129 @@ pub fn sign_in() -> Html {
                 state.password = password;
                 log!("password", state.password.clone());
             });
-    html! {
+    let component = html! {
       <div
+        id="root"
         style="
-          width: 100%;
-          height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+          display: block;
+          color: rgba(0, 53, 0, 0.87);
+          margin: 0;
+          font-size: 0.875rem;
+          font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+          font-weight: 400;
+          line-height: 1.43;
+          letter-spacing: 0.01071em;
+          background-color: #fff;
+          -webkit-font-smoothing: antialiased;
+          box-sizing: inherit;
         "
       >
         <div
-            id="root"
-            style="
-              display: block;
-              color: rgba(0, 53, 0, 0.87);
-              margin: 0;
-              font-size: 0.875rem;
-              font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-              font-weight: 400;
-              line-height: 1.43;
-              letter-spacing: 0.01071em;
-              background-color: #fff;
-              -webkit-font-smoothing: antialiased;
-              box-sizing: inherit;
-            "
+          style="
+            max-width: 444px;
+            width: 100%;
+            display: block;
+            box-sizing: border-box;
+            margin-left: auto;
+            margin-right: auto;
+            padding-left: 16px;
+            padding-right: 16px;
+          "
         >
-            <div
+          <div
+            style="
+              display: flex;
+              margin-top: 64px;
+              align-items: center;
+              flex-direction: column;
+            "
+          >
+            <SvgIconWrapper/>
+            <h1
               style="
-                max-width: 444px;
-                width: 100%;
+                font-size: 1.5rem;
+                font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+                font-weight: 400;
+                line-height: 1.334;
+                letter-spacing: 0em;
+                margin: 0;
+                box-sizing: inherit;
                 display: block;
-                box-sizing: border-box;
-                margin-left: auto;
-                margin-right: auto;
-                padding-left: 16px;
-                padding-right: 16px;
+                font-size: 2em;
+                margin-block-start: 0.67em;
+                margin-block-end: 0.67em;
+                margin-inline-start: 0px;
+                margin-inline-end: 0px;
+                font-weight: bold;
+              "
+            >
+              {header_name}
+            </h1>
+            <form
+              onsubmit={handle_form_submit}
+              novalidate=true
+              style="
+                width: 100%;
+                margin-top: 24px;
+                box-sizing: inherit;
+                display: block;
               "
             >
               <div
                 style="
+                  width: calc(100% + 16px);
+                  margin: -8px;
                   display: flex;
-                  margin-top: 64px;
-                  align-items: center;
-                  flex-direction: column;
+                  flex-wrap: wrap;
+                  box-sizing: border-box;
                 "
               >
-                <SvgIconWrapper/>
-                <h1
-                  style="
-                    font-size: 1.5rem;
-                    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                    font-weight: 400;
-                    line-height: 1.334;
-                    letter-spacing: 0em;
-                    margin: 0;
-                    box-sizing: inherit;
-                    display: block;
-                    font-size: 2em;
-                    margin-block-start: 0.67em;
-                    margin-block-end: 0.67em;
-                    margin-inline-start: 0px;
-                    margin-inline-end: 0px;
-                    font-weight: bold;
-                  "
-                >
-                  {header_name}
-                </h1>
-                <form
-                  onsubmit={handle_form_submit}
-                  novalidate=true
-                  style="
-                    width: 100%;
-                    margin-top: 24px;
-                    box-sizing: inherit;
-                    display: block;
-                  "
-                >
-                  <div
-                    style="
-                      width: calc(100% + 16px);
-                      margin: -8px;
-                      display: flex;
-                      flex-wrap: wrap;
-                      box-sizing: border-box;
-                    "
-                  >
-                    <InputForm placeholder={"Login".to_owned()} input_type={HtmlInputType::Text} action={handle_username_change} />
-                    <InputForm placeholder={"Password".to_owned()} input_type={HtmlInputType::Password} action={handle_password_change.clone()} />
-                  </div>
-                  <div>
-                    <SubmitButton placeholder={header_name}/>
-                  </div>
-                  <div
-                    style="
-                      justify-content: flex-end;
-                      width: 100%;
-                      display: flex;
-                      flex-wrap: wrap;
-                      box-sizing: border-box;
-                    "
-                  >
-                    <div
-                      style="
-                        margin: 0;
-                        box-sizing: border-box;
-                        display: block;
-                      "
-                    >
-                      <a
-                        href=""
-                        style="
-                          text-decoration: none;
-                          color: #556cd6;
-                          font-size: 0.875rem;
-                          font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-                          font-weight: 400;
-                          line-height: 1.43;
-                          letter-spacing: 0.01071em;
-                          margin: 0;
-                          box-sizing: inherit;
-                          cursor: pointer;
-                        "
-                      >
-                      {"Don't have an account? "}<Link<Routes> to={Routes::SignUp}>{ "Sign up" }</Link<Routes>>
-                      </a>
-                    </div>
-                  </div>
-                </form>
+                <InputForm placeholder={"Login".to_owned()} input_type={HtmlInputType::Text} action={handle_username_change} />
+                <InputForm placeholder={"Password".to_owned()} input_type={HtmlInputType::Password} action={handle_password_change.clone()} />
               </div>
-            </div>
-            <Link<Routes> to={Routes::Secure}>{ "Go to Secure" }</Link<Routes>>
+              <div>
+                <SubmitButton placeholder={header_name}/>
+              </div>
+              <div
+                style="
+                  justify-content: flex-end;
+                  width: 100%;
+                  display: flex;
+                  flex-wrap: wrap;
+                  box-sizing: border-box;
+                "
+              >
+                <div
+                  style="
+                    margin: 0;
+                    box-sizing: border-box;
+                    display: block;
+                  "
+                >
+                  <a
+                    href=""
+                    style="
+                      text-decoration: none;
+                      color: #556cd6;
+                      font-size: 0.875rem;
+                      font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+                      font-weight: 400;
+                      line-height: 1.43;
+                      letter-spacing: 0.01071em;
+                      margin: 0;
+                      box-sizing: inherit;
+                      cursor: pointer;
+                    "
+                  >
+                    {"Don't have an account? "}<Link<Routes> to={Routes::SignUp}>{ "Sign up" }</Link<Routes>>
+                  </a>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-    }
+        <Link<Routes> to={Routes::Secure}>{ "Go to Secure" }</Link<Routes>>
+      </div>
+    };
+    html! {<CenterFormWrapper inner_html={component}/>}
 }
