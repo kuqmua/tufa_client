78d570aedebd96afef06cda4979266f56941ebf2
kuqmua <serezhashmakov@outlook.com>
2022-05-29 13:01:58 +0300

    lil cleaning

--- src/components/header/buttons/menu_button.rs
+++ src/components/header/buttons/menu_button.rs
@@ -1,5 +1,4 @@
-// use crate::components::header::buttons::button_wrapper::ButtonWrapper;
-// use crate::components::svg::menu::Menu;
+use crate::components::svg::menu::Menu;
 use crate::constants::HEADER_ICONS_COLOR;
 use web_sys::MouseEvent;
 use yew::{function_component, html, Callback, Properties};
@@ -13,7 +12,6 @@ pub struct MenuButtonProps {
 #[function_component(MenuButton)]
 pub fn menu_button(props: &MenuButtonProps) -> Html {
     let icon_size = "26px".to_owned();
-    // let html_handle = html! {<Menu height={icon_size.clone()} width={icon_size} fill={HEADER_ICONS_COLOR.to_owned()}/>};
     let size_px: u32 = 26;
     let style_handle = format!(
       "
@@ -36,18 +34,7 @@ pub fn menu_button(props: &MenuButtonProps) -> Html {
           aria-controls="drawer-name-left" 
           aria-expanded="false"
         >
-          <svg 
-            xmlns="http://www.w3.org/2000/svg" 
-            viewBox="0 0 24 24" 
-            height={icon_size.clone()} 
-            width={icon_size.clone()} 
-            fill={HEADER_ICONS_COLOR}
-          >
-            <path d="M0 0h24v24H0V0z" fill="none"/>
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
-          </svg>   
+          <Menu height={icon_size.clone()} width={icon_size} fill={HEADER_ICONS_COLOR.to_owned()}/> 
         </button>
     }
 }
-
-// <ButtonWrapper inner_html={html_handle} callback={props.callback.clone()} id={"person_outline_button".to_owned()}/>
--- src/components/test_drawer.rs
+++ src/components/test_drawer.rs
@@ -14,59 +14,60 @@ pub struct TestDrawerProps {
 
 #[function_component(TestDrawer)]
 pub fn test_drawer(props: &TestDrawerProps) -> Html {
+    //todo: add esc keydown handling support(from working drawer.html) 
     let section_style = format!(
-        "
-          display: {};
-        ",
-        props.drawer_is_active_display_value.clone()
-      );
-      let drawer_overlay_style = format!(
-        "
-          position: fixed;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          width: 100%;
-          z-index: 200;
-          opacity: 0;
-          transition: opacity 0.3s;
-          will-change: opacity;
-          background-color: #000;
-          -webkit-user-select: none;
-          -moz-user-select: none;
-          -ms-user-select: none;
-          user-select: none; 
-          opacity: {};
-        ",
-        props.drawer_overlay_opacity.clone()
-      );
-      let drawer_wrapper_style = format!(
-        "
-          position: fixed;
-          top: 0;
-          left: 0;
-          right: auto;
-          bottom: 0;
-          height: 100%;
-          width: 100%;
-          max-width: 500px;
-          z-index: 9999;
-          overflow: auto;
-          transition: transform 0.3s;
-          will-change: transform;
-          background-color: {};
-          display: flex;
-          flex-direction: column; 
-          -webkit-overflow-scrolling: touch; /* enables momentum scrolling in iOS overflow elements */
-          box-shadow: 0 2px 6px black;
-          -webkit-transform: {};
-          transform: {};
-        ",
-        BACKGROUND_COLOR,
-        props.drawer_wrapper_webkit_transform.clone(),
-        props.drawer_wrapper_transform.clone()
-      );
+      "
+        display: {};
+      ",
+      props.drawer_is_active_display_value.clone()
+    );
+    let drawer_overlay_style = format!(
+      "
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        z-index: 200;
+        opacity: 0;
+        transition: opacity 0.3s;
+        will-change: opacity;
+        background-color: #000;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none; 
+        opacity: {};
+      ",
+      props.drawer_overlay_opacity.clone()
+    );
+    let drawer_wrapper_style = format!(
+      "
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: auto;
+        bottom: 0;
+        height: 100%;
+        width: 100%;
+        max-width: 500px;
+        z-index: 9999;
+        overflow: auto;
+        transition: transform 0.3s;
+        will-change: transform;
+        background-color: {};
+        display: flex;
+        flex-direction: column; 
+        -webkit-overflow-scrolling: touch; /* enables momentum scrolling in iOS overflow elements */
+        box-shadow: 0 2px 6px black;
+        -webkit-transform: {};
+        transform: {};
+      ",
+      BACKGROUND_COLOR,
+      props.drawer_wrapper_webkit_transform.clone(),
+      props.drawer_wrapper_transform.clone()
+    );
     html! {
       <>
         <section 
