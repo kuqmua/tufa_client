6c8c370564e8c5639b3a843a07fdcd259ce03b74
kuqmua <serezhashmakov@outlook.com>
2022-05-31 22:35:17 +0300

    return of old menu button

--- src/components/header/buttons/menu_button.rs
+++ src/components/header/buttons/menu_button.rs
@@ -1,5 +1,5 @@
+use crate::components::header::buttons::button_wrapper::ButtonWrapper;
 use crate::components::svg::menu::Menu;
-use crate::constants::BACKGROUND_COLOR;
 use crate::constants::HEADER_ICONS_COLOR;
 use web_sys::MouseEvent;
 use yew::{function_component, html, Callback, Properties};
@@ -12,26 +12,8 @@ pub struct MenuButtonProps {
 #[function_component(MenuButton)]
 pub fn menu_button(props: &MenuButtonProps) -> Html {
     let icon_size = "26px".to_owned();
-    let size_px: u32 = 26;
-    let style_handle = format!(
-        "
-        width: {}px;
-        height: {}px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        background-color: {};
-        border: 1px solid {};
-        padding: 0px;
-      ",
-        size_px, size_px, BACKGROUND_COLOR, BACKGROUND_COLOR
-    );
+    let html_handle = html! {<Menu height={icon_size.clone()} width={icon_size} fill={HEADER_ICONS_COLOR.to_owned()}/>};
     html! {
-        <button
-          style={style_handle}
-          onclick={props.callback.clone()}
-        >
-          <Menu height={icon_size.clone()} width={icon_size} fill={HEADER_ICONS_COLOR.to_owned()}/>
-        </button>
+      <ButtonWrapper inner_html={html_handle} callback={props.callback.clone()} id={"menu_button".to_owned()}/>
     }
 }
