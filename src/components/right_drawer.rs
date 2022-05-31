9d8ca3e1235ec4fc2e4082284fe93a4d48c1e387
kuqmua <serezhashmakov@outlook.com>
2022-05-31 19:00:09 +0300

    step to RightDrawer

---
+++ src/components/right_drawer.rs
@@ -0,0 +1,145 @@
+#[derive(Debug, PartialEq)]
+pub enum RightDrawerChangingStyleState {
+  Initial,
+  OpenedBeforeTimeout,
+  OpenedAfterTimeout,
+  ClosedBeforeTimeout,
+}
+
+impl Clone for RightDrawerChangingStyleState {
+  fn clone(&self) -> Self {
+    match *self {
+        RightDrawerChangingStyleState::Initial => RightDrawerChangingStyleState::Initial,
+        RightDrawerChangingStyleState::OpenedBeforeTimeout => RightDrawerChangingStyleState::OpenedBeforeTimeout,
+        RightDrawerChangingStyleState::OpenedAfterTimeout => RightDrawerChangingStyleState::OpenedAfterTimeout,
+        RightDrawerChangingStyleState::ClosedBeforeTimeout => RightDrawerChangingStyleState::ClosedBeforeTimeout,
+    }
+  }
+}
+
+impl RightDrawerChangingStyleState {
+  pub fn get_value(&self) -> RightDrawerChangingStyle {
+    match *self {
+        RightDrawerChangingStyleState::Initial => RightDrawerChangingStyle {
+          display: String::from("none"),
+          transform: String::from("translate3d(-100%, 0, 0)"),
+          webkit_transform: String::from("translate3d(-100%, 0, 0)"),
+          opacity: String::from(""),
+        },
+        RightDrawerChangingStyleState::OpenedBeforeTimeout => RightDrawerChangingStyle {
+          display: String::from("block"),
+          transform: String::from("translate3d(-100%, 0, 0)"),
+          webkit_transform: String::from("translate3d(-100%, 0, 0)"),
+          opacity: String::from(""),
+        },
+        RightDrawerChangingStyleState::OpenedAfterTimeout => RightDrawerChangingStyle {
+          display: String::from("block"),
+          transform: String::from("translate3d(0, 0, 0)"),
+          webkit_transform: String::from("translate3d(0, 0, 0)"),
+          opacity: String::from("0.5"),
+        },
+        RightDrawerChangingStyleState::ClosedBeforeTimeout => RightDrawerChangingStyle {
+          display: String::from("block"),
+          transform: String::from("translate3d(-100%, 0, 0)"),
+          webkit_transform: String::from("translate3d(-100%, 0, 0)"),
+          opacity: String::from(""),
+        },
+    }
+  }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct RightDrawerChangingStyle {
+  pub display: String,
+  pub transform: String,
+  pub webkit_transform: String,
+  pub opacity: String,
+}
+
+use web_sys::MouseEvent;
+use yew::{function_component, html, Properties, Callback};
+use crate::constants::BACKGROUND_COLOR;
+use crate::constants::FEED_WIDTH_PX;
+
+#[derive(Properties, PartialEq)]
+pub struct RightDrawerProps {
+    pub callback: Callback<MouseEvent>,
+    pub style_state: RightDrawerChangingStyleState,
+}
+
+#[function_component(RightDrawer)]
+pub fn drawer(props: &RightDrawerProps) -> Html {
+    //todo: add esc keydown handling support(from working drawer.html) 
+    let changing_style = props.style_state.get_value();
+    let section_style = format!(
+      "
+        display: {};
+      ",
+      changing_style.display
+    );
+    let drawer_overlay_style = format!(
+      "
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        z-index: 200;
+        opacity: 0;
+        transition: opacity 0.3s;
+        will-change: opacity;
+        background-color: #000;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none; 
+        opacity: {};
+      ",
+      changing_style.opacity
+    );
+    let drawer_wrapper_style = format!(
+      "
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: auto;
+        bottom: 0;
+        height: 100%;
+        width: 100%;
+        width: {}px;
+        z-index: 9999;
+        overflow: auto;
+        transition: transform 0.3s;
+        will-change: transform;
+        background-color: {};
+        display: flex;
+        flex-direction: column; 
+        -webkit-overflow-scrolling: touch; /* enables momentum scrolling in iOS overflow elements */
+        -webkit-transform: {};
+        transform: {};
+      ",
+      FEED_WIDTH_PX,
+      BACKGROUND_COLOR,
+      changing_style.webkit_transform,
+      changing_style.transform
+    );
+    html! {
+      <>
+        <section 
+          style={section_style}
+        >
+          <div 
+            data-drawer-close="data-drawer-close" 
+            style={drawer_overlay_style}
+            onclick={&props.callback}
+          >
+          </div>
+          <div 
+            style={drawer_wrapper_style}
+          >
+          </div>
+        </section>
+      </>
+    }
+}
