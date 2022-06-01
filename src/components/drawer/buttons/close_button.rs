811cffe3268412394eb8fe5d2fe60fb7848c8da1
kuqmua <serezhashmakov@outlook.com>
2022-06-01 20:34:02 +0300

    drawer close button

---
+++ src/components/drawer/buttons/close_button.rs
@@ -0,0 +1,19 @@
+use crate::components::header::buttons::button_wrapper::ButtonWrapper;
+use crate::components::svg::close_black::CloseBlack;
+use crate::constants::HEADER_ICONS_COLOR;
+use web_sys::MouseEvent;
+use yew::{function_component, html, Callback, Properties};
+
+#[derive(Properties, PartialEq)]
+pub struct CloseButtonProps {
+    pub callback: Callback<MouseEvent>,
+}
+
+#[function_component(CloseButton)]
+pub fn close_button(props: &CloseButtonProps) -> Html {
+    let icon_size = "26px".to_owned();
+    let html_handle = html! {<CloseBlack height={icon_size.clone()} width={icon_size} fill={HEADER_ICONS_COLOR.to_owned()}/>};
+    html! {
+      <ButtonWrapper inner_html={html_handle} callback={props.callback.clone()} id={"menu_button".to_owned()}/>
+    }
+}
