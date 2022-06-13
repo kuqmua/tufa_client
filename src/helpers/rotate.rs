7cbb9ea5f7e9392a6cb0c90e1cb67a1e83b298e3
kuqmua <serezhashmakov@outlook.com>
2022-06-13 17:44:27 +0300

    additional rotate methods

--- src/helpers/rotate.rs
+++ src/helpers/rotate.rs
@@ -4,12 +4,19 @@ pub struct Rotate {
 }
 
 impl Rotate {
-    pub fn change(possible_degrees: u16) -> Result<Self, u16> {
+    pub fn new(possible_degrees: u16) -> Result<Self, u16> {
         if possible_degrees > 360 {
             return Err(possible_degrees);
         }
         Ok(Self {
             degrees: possible_degrees
         })
+    }
+    pub fn change(mut self, possible_degrees: u16) -> Result<Self, Self> {
+        if possible_degrees > 360 {
+            return Err(self);
+        }
+        self.degrees = possible_degrees;
+        Ok(self)
     }
 }
