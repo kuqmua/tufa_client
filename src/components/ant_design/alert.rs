72ef4219477083ec6d6e3c658181458ca05de7da
kuqmua <serezhashmakov@outlook.com>
2022-06-18 16:05:39 +0300

    lil alert template

--- src/components/ant_design/alert.rs
+++ src/components/ant_design/alert.rs
@@ -1,1 +1,23 @@
+// use gloo::worker::Callback;
+// use yew::{function_component, html, Html, Properties};
 
+// #[derive(Properties, PartialEq)]
+// pub struct IconProps {
+//     pub after_close Callback<()>,
+//     pub banner: bool,
+// closable	Whether Alert can be closed	boolean	-
+// closeText	Close text to show	string|ReactNode	-
+// description	Additional content of Alert	string|ReactNode	-
+// icon	Custom icon, effective when showIcon is true	ReactNode	-	3.10.0
+// message	Content of Alert	string|ReactNode	-
+// showIcon	Whether to show icon	boolean	false, in banner mode default is true
+// type	Type of Alert styles, options: success, info, warning, error	string	info, in banner mode default is warning
+// onClose	Callback when Alert is closed	(e: MouseEvent) => void
+// }
+
+// #[function_component(Icon)]
+// pub fn icon(props: &IconProps) -> Html {
+//     html! {
+
+//     }
+// }
