41da6aaa6a60a126efc86c993a0354f1de516f79
kuqmua <serezhashmakov@outlook.com>
2022-06-18 20:19:41 +0300

    icon additional class

--- src/components/ant_design/icon.rs
+++ src/components/ant_design/icon.rs
@@ -4,6 +4,7 @@ use yew::{function_component, html, Html, Properties};
 pub struct IconProps {
     pub inner_html: Html,      //svg icon type
     pub style: Option<String>, //CSSProperties
+    pub additional_class: Option<String>,
     // pub theme: Option<Theme>,//explicit svg styles
     // moved into svg
     // pub component: ComponentType<CustomIconComponentProps>//todo
@@ -13,8 +14,13 @@ pub struct IconProps {
 
 #[function_component(Icon)]
 pub fn icon(props: &IconProps) -> Html {
+    let additional_class = match props.additional_class.clone() {
+        None => String::from(""),
+        Some(class) => class,
+    };
+    let class = format!("anticon {}", additional_class);
     html! {
-        <i class="anticon">//todo: aria-label="icon: home" (example)
+        <i class={class}>//todo: aria-label="icon: home" (example)
           {props.inner_html.clone()}
         </i>
     }
