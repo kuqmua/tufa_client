080ce85e27fb00ef84402ce2f5d36dc2fc97c181
kuqmua <serezhashmakov@outlook.com>
2022-06-20 21:20:47 +0300

    fn preps

--- src/components/ant_design/alert.rs
+++ src/components/ant_design/alert.rs
@@ -8,7 +8,7 @@ use crate::components::ant_design::svg::helpers::theme::Theme;
 use crate::components::ant_design::svg::info_circle::InfoCircle;
 use colorsys::Hsl;
 use web_sys::MouseEvent;
-use yew::{function_component, html, Callback, Html, Properties};
+use yew::{function_component, html, Callback, Html, Properties, use_state};
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum AlertType {
@@ -40,11 +40,34 @@ pub struct AlertProps {
     pub message: Option<String>,
     pub show_icon: Option<()>,
     pub type_handle: Option<AlertType>,
-    pub on_close: Option<Callback<MouseEvent>>,
+    pub on_close: Option<Callback<()>>,
 }
 
 #[function_component(Alert)]
 pub fn alert(props: &AlertProps) -> Html {
+    let closing = use_state(|| false);
+    let closed = use_state(|| false);
+    let style = use_state(|| String::from(""));
+    let closing_handle_close_clone = closing.clone();
+    let on_close_clone = props.on_close.clone();
+    let handle_close = Callback::<MouseEvent>::from(move |e: MouseEvent| {
+        e.prevent_default();
+        // const dom = ReactDOM.findDOMNode(this) as HTMLElement;
+        // dom.style.height = "${dom.offsetHeight}px";
+        // Magic code
+        // 重复一次后才能正确设置 height
+        // dom.style.height = `${dom.offsetHeight}px`;
+        closing_handle_close_clone.set(true);
+        // if let Some(function) = on_close_clone {
+        //     // function();
+        // }
+        // match props.on_close.clone() {
+        //     None => Callback::<MouseEvent>::from(move |_| {}),
+        //     Some(_) => Callback::<MouseEvent>::from(move |_| {}),
+        // };
+        // (this.props.onClose || noop)(e);
+    });
+    //
     let message = match props.message.clone() {
         None => String::from(""),
         Some(msg) => msg,
@@ -71,15 +94,11 @@ pub fn alert(props: &AlertProps) -> Html {
         description_class,
         show_icon_class
     );
-    let on_close = match props.on_close.clone() {
-        None => Callback::<MouseEvent>::from(move |_| {}),
-        Some(_) => Callback::<MouseEvent>::from(move |_| {}),
-    };
     let close_button = match props.closable {
         None => match props.close_text.clone() {
             None => html! {},
             Some(text) => html! {
-                <button onclick={on_close} type="button" class="ant-alert-close-icon" tabindex="0">
+                <button onclick={handle_close} type="button" class="ant-alert-close-icon" tabindex="0">
                   <span class="ant-alert-close-text">{text}</span></button>
             },
         },
@@ -89,13 +108,13 @@ pub fn alert(props: &AlertProps) -> Html {
                     html! {<Close fill={FillWith::Hsl(Hsl::new(0.0, 100.0, 0.0, Some(1.0)))}/>};
                 let icon_inner_html = html! {<Icon inner_html={close} additional_class={String::from("anticon-close")} />};
                 html! {
-                  <button onclick={on_close} type="button" class="ant-alert-close-icon" tabindex="0">
+                  <button type="button" class="ant-alert-close-icon" tabindex="0">// onclick={on_close}
                     {icon_inner_html}
                   </button>
                 }
             }
             Some(text) => html! {
-                <button onclick={on_close} type="button" class="ant-alert-close-icon" tabindex="0">
+                <button type="button" class="ant-alert-close-icon" tabindex="0">// onclick={on_close}
                   <span class="ant-alert-close-text">{text}</span></button>
             },
         },
@@ -131,8 +150,9 @@ pub fn alert(props: &AlertProps) -> Html {
             }
         }
     };
+    let on_close_clone = &*style;
     html! {
-      <div data-show="true" class={class}>
+      <div data-show="true" class={class} style={on_close_clone.clone()}>
         {icon}
         <span class="ant-alert-message">
           {message}
