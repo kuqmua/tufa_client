a791acf636be278c05269cb222bcad2bff5ad415
kuqmua <serezhashmakov@outlook.com>
2022-06-26 11:45:19 +0300

    SvgProps AttrValue

--- src/components/ant_design/svg/helpers/svg_props.rs
+++ src/components/ant_design/svg/helpers/svg_props.rs
@@ -1,12 +1,13 @@
 use crate::components::ant_design::svg::helpers::fill_with::FillWith;
 use crate::components::ant_design::svg::helpers::theme::Theme;
 use crate::helpers::rotate::Rotate;
+use yew::virtual_dom::AttrValue;
 use yew::Properties;
 
 #[derive(Properties, PartialEq)]
 pub struct SvgProps {
-    pub height: Option<String>,
-    pub width: Option<String>,
+    pub height: Option<AttrValue>,
+    pub width: Option<AttrValue>,
     pub fill: Option<FillWith>,
     pub spin: Option<()>,
     pub rotate: Option<Rotate>,
