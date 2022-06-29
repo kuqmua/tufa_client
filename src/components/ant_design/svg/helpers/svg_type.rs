7289f83dc88d53bf3e1e694d9efd14f814d20c73
kuqmua <serezhashmakov@outlook.com>
2022-06-29 18:15:01 +0300

    solve doubts about imports

--- src/components/ant_design/svg/helpers/svg_type.rs
+++ src/components/ant_design/svg/helpers/svg_type.rs
@@ -1,26 +1,3 @@
-// use crate::components::ant_design::svg::check_circle::CheckCircle;
-// use crate::components::ant_design::svg::close::Close;
-// use crate::components::ant_design::svg::close_circle::CloseCircle;
-// use crate::components::ant_design::svg::cloud::Cloud;
-// use crate::components::ant_design::svg::copy::Copy;
-// use crate::components::ant_design::svg::dislike::Dislike;
-// use crate::components::ant_design::svg::down::Down;
-// use crate::components::ant_design::svg::exclamation_circle::ExclamationCircle;
-// use crate::components::ant_design::svg::github::Github;
-// use crate::components::ant_design::svg::heart::Heart;
-// use crate::components::ant_design::svg::info_circle::InfoCircle;
-// use crate::components::ant_design::svg::like::Like;
-// use crate::components::ant_design::svg::loading::Loading;
-// use crate::components::ant_design::svg::login::Login;
-// use crate::components::ant_design::svg::logout::Logout;
-// use crate::components::ant_design::svg::reddit::Reddit;
-// use crate::components::ant_design::svg::share_alt::ShareAlt;
-// use crate::components::ant_design::svg::sync::Sync;
-// use crate::components::ant_design::svg::twitter::Twitter;
-// use crate::components::ant_design::svg::up::Up;
-// use crate::components::ant_design::svg::user::User;
-// use crate::components::ant_design::svg::youtube::Youtube;
-//have some doubts about import generation from SvgComponent
 use crate::components::ant_design::svg::helpers::svg_component_trait::SvgComponent;
 use crate::components::ant_design::svg::helpers::svg_props::SvgProps;
 use yew::html;
