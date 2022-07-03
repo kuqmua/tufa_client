3b5824fca4790bf19c5b37058d4740da15bb7f6a
kuqmua <serezhashmakov@outlook.com>
2022-07-03 20:31:46 +0300

    bad refactoring antd badge

--- src/components/ant_design/badge.rs
+++ src/components/ant_design/badge.rs
@@ -11,35 +11,48 @@ pub struct BadgeOffset {
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum BadgeStatus {
-    Success,
-    Processing,
-    Default,
-    Error,
-    Warning,
+    Success(BadgeStatusText),
+    Processing(BadgeStatusText),
+    Default(BadgeStatusText),
+    Error(BadgeStatusText),
+    Warning(BadgeStatusText),
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct BadgeStatusText {
+    pub text: Option<String>,
 }
 
 impl BadgeStatus {
-    pub fn get_class(&self) -> String {
+    pub fn get_sup_class(&self) -> String {
         match self {
-            BadgeStatus::Success => String::from("ant-badge-status-success"),
-            BadgeStatus::Processing => String::from("ant-badge-status-processing"),
-            BadgeStatus::Default => String::from("ant-badge-status-default"),
-            BadgeStatus::Error => String::from("ant-badge-status-error"),
-            BadgeStatus::Warning => String::from("ant-badge-status-warning"),
+            BadgeStatus::Success(_) => String::from("ant-badge-status-success"),
+            BadgeStatus::Processing(_) => String::from("ant-badge-status-processing"),
+            BadgeStatus::Default(_) => String::from("ant-badge-status-default"),
+            BadgeStatus::Error(_) => String::from("ant-badge-status-error"),
+            BadgeStatus::Warning(_) => String::from("ant-badge-status-warning"),
         }
     }
+    pub fn get_span_class(&self) -> String {
+        String::from("ant-badge-status")
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct BadgeDot {
+    pub status: Option<BadgeStatus>,
 }
 
 #[derive(Properties, PartialEq)]
 pub struct BadgeProps {
     pub color: Option<Hsl>,          // Customize Badge dot color	string	-	3.16.0
     pub count: Option<u64>,          //	Number to show in badge	ReactNode
-    pub dot: Option<()>,             // Whether to display a red dot instead of count	boolean	false
+    pub dot: Option<BadgeDot>,       // Whether to display a red dot instead of count	boolean	false
     pub offset: Option<BadgeOffset>, //	set offset of the badge dot, like[x, y]	[number, number]	-
     pub overflow_count: Option<u64>, //dont think it would be usefull//	Max count to show	number	99
     pub show_zero: Option<()>,       //	Whether to show badge when count is zero	boolean	false
-    pub status: Option<BadgeStatus>, //	Set Badge as a status dot	success | processing | default | error | warning	''
-    pub text: Option<String>, //	If status is set, text sets the display text of the status dot	string	''
+    // pub status: Option<BadgeStatus>, //	Set Badge as a status dot	success | processing | default | error | warning	''
+    // pub text: Option<String>, //	If status is set, text sets the display text of the status dot	string	''
     pub title: Option<String>, //	Text to show when hovering over the badge	string	count
     #[prop_or_default]
     pub children: Children,
@@ -61,180 +74,158 @@ pub fn badge(props: &BadgeProps) -> Html {
             format!("{} {}", right_or_left, margin_top_or_bottom)
         }
     };
-    let status_dot_class = match props.status.clone() {
-        None => String::from(""),
-        Some(status) => status.get_class(),
+    let (status_sup_class, status_span_class) = match props.dot.clone() {
+        None => (String::from(""), String::from("")),
+        Some(dot) => match dot.status.clone() {
+            None => (String::from(""), String::from("")),
+            Some(status) => (status.get_sup_class(), status.get_span_class()),
+        },
     };
-    let status_class = match props.status.clone() {
-        None => String::from(""),
-        Some(_) => String::from("ant-badge-status"),
-    };
-    let text_html = match (&props.text, props.status.clone()) {
-        (None, None) => html! {},
-        (None, Some(_)) => html! {},
-        (Some(_), None) => html! {},
-        (Some(text), Some(_)) => html! {<span class="ant-badge-status-text">{text}</span>},
-    };
+    // let text_html = match props.dot.clone() {
+    //     None => html! {},
+    //     Some(dot) => match dot.status.clone() {
+    //         None => html! {},
+    //         Some(status) => match status {
+    //             BadgeStatus::Success(status_text) => match status_text.text {
+    //                 None => html! {},
+    //                 Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+    //             },
+    //             BadgeStatus::Processing(status_text) => match status_text.text {
+    //                 None => html! {},
+    //                 Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+    //             },
+    //             BadgeStatus::Default(status_text) => match status_text.text {
+    //                 None => html! {},
+    //                 Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+    //             },
+    //             BadgeStatus::Error(status_text) => match status_text.text {
+    //                 None => html! {},
+    //                 Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+    //             },
+    //             BadgeStatus::Warning(status_text) => match status_text.text {
+    //                 None => html! {},
+    //                 Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+    //             },
+    //         },
+    //     },
+    // };
     let title = match (props.title.clone(), props.count) {
         (None, None) => String::from(""),
         (None, Some(count)) => count.to_string(),
         (Some(title), None) => title,
         (Some(title), Some(_)) => title,
     };
-    let sup = match props.count {
-        None => match (&props.color, &props.dot, &props.status) {
-            (None, None, None) => html! {},
-            (None, None, Some(_)) => {
-                html! { <sup data-show="true" class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)} style={offset_style} title={title}></sup> }
+    let color_style = match props.color.clone() {
+        None => String::from(""),
+        Some(color) => format!("background: {};", color.to_css_string()),
+    };
+    let sup_style = format!("{} {}", color_style, offset_style);
+    let sup_class = format!("ant-scroll-number ant-badge-dot {}", status_sup_class); //ant-scroll-number ant-badge-count ant-badge-multiple-words
+    let span_class = format!("ant-badge {}", status_span_class);
+    let dot = match &props.dot {
+        None => html! {},
+        Some(dot) => match dot.status.clone() {
+            None => {
+                html! { <sup data-show="true" class={sup_class.clone()} style={sup_style.clone()} title={title.clone()}></sup> }
             }
-            (None, Some(_), None) => {
-                html! { <sup data-show="true" class="ant-scroll-number ant-badge-dot" style={offset_style} title={title}></sup> }
-            }
-            (None, Some(_), Some(_)) => {
-                html! { <sup data-show="true" class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)} style={offset_style} title={title}></sup> }
-            }
-            (Some(color), None, None) => {
-                html! { <sup data-show="true" class="ant-scroll-number ant-badge-dot" style={format!("background: {}; {}", color.to_css_string(), offset_style)} title={title}></sup> }
-            }
-            (Some(color), None, Some(_)) => html! {
-               <sup
-                 data-show="true"
-                 class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)}
-                 style={format!("background: {}; {}", color.to_css_string(), offset_style)}
-                 title={title}
-               >
-               </sup>
+            Some(status) => match status {
+                BadgeStatus::Success(status_text) => match status_text.text {
+                    None => {
+                        html! { <sup data-show="true" class={sup_class.clone()} style={sup_style.clone()} title={title.clone()}></sup> }
+                    }
+                    Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+                },
+                BadgeStatus::Processing(status_text) => match status_text.text {
+                    None => {
+                        html! { <sup data-show="true" class={sup_class.clone()} style={sup_style.clone()} title={title.clone()}></sup> }
+                    }
+                    Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+                },
+                BadgeStatus::Default(status_text) => match status_text.text {
+                    None => {
+                        html! { <sup data-show="true" class={sup_class.clone()} style={sup_style.clone()} title={title.clone()}></sup> }
+                    }
+                    Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+                },
+                BadgeStatus::Error(status_text) => match status_text.text {
+                    None => {
+                        html! { <sup data-show="true" class={sup_class.clone()} style={sup_style.clone()} title={title.clone()}></sup> }
+                    }
+                    Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+                },
+                BadgeStatus::Warning(status_text) => match status_text.text {
+                    None => {
+                        html! { <sup data-show="true" class={sup_class.clone()} style={sup_style.clone()} title={title.clone()}></sup> }
+                    }
+                    Some(text) => html! {<span class="ant-badge-status-text">{text}</span>},
+                },
             },
-            (Some(color), Some(_), None) => {
-                html! { <sup data-show="true" class="ant-scroll-number ant-badge-dot" style={format!("background: {}; {}", color.to_css_string(), offset_style)} title={title}></sup> }
-            }
-            (Some(color), Some(_), Some(_)) => {
-                html! {
-                <sup
-                  data-show="true"
-                  class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)}
-                  style={format!("background: {}; {}", color.to_css_string(), offset_style)}
-                  title={title}
-                >
-                </sup> }
+        },
+    };
+    let sup = match props.count {
+        None => dot,
+        Some(count) => match props.dot {
+            Some(_) => dot,
+            None => {
+                let max_count_number = props.overflow_count.unwrap_or(99);
+                let count_to_show = count.to_string();
+                let max_count_number_text = format!("{}+", max_count_number);
+                let should_render = match (count == 0, props.show_zero) {
+                    (true, None) => false,
+                    (true, Some(_)) => false,
+                    (false, None) => true,
+                    (false, Some(_)) => true,
+                };
+                let numbers = count_to_show
+                    .chars()
+                    .map(|char| match Numeric::try_from(char) {
+                        Err(char) => {
+                            error!("badge component char is not a numeric: ", char.to_string());
+                            html! {}
+                        }
+                        Ok(numeric) => html! {
+                            <BadgeNumbers numeric={numeric}/>
+                        },
+                    })
+                    .collect::<Vec<Html>>();
+                let is_max_count_number_less = count > max_count_number;
+                if count == 0 && props.show_zero.is_none() {
+                    html! {}
+                } else if is_max_count_number_less {
+                    html! {
+                      <sup
+                        data-show="true"
+                        class={sup_class.clone()}
+                        title={title.clone()}
+                        style={sup_style.clone()}
+                      >
+                        {max_count_number_text}
+                      </sup>
+                    }
+                } else {
+                    html! {
+                      <>
+                        if should_render {
+                          <sup
+                            data-show="true"
+                            class={sup_class}
+                            title={title}
+                            style={sup_style}
+                          >
+                            {for numbers}
+                          </sup>
+                        }
+                      </>
+                    }
+                }
             }
         },
-        Some(count) => {
-            let max_count_number = props.overflow_count.unwrap_or(99);
-            let count_to_show = count.to_string();
-            let max_count_number_text = format!("{}+", max_count_number);
-            let should_render = match (count == 0, props.show_zero) {
-                (true, None) => false,
-                (true, Some(_)) => false,
-                (false, None) => true,
-                (false, Some(_)) => true,
-            };
-            let numbers = count_to_show
-                .chars()
-                .map(|char| match Numeric::try_from(char) {
-                    Err(char) => {
-                        error!("badge component char is not a numeric: ", char.to_string());
-                        html! {}
-                    }
-                    Ok(numeric) => html! {
-                        <BadgeNumbers numeric={numeric}/>
-                    },
-                })
-                .collect::<Vec<Html>>();
-            let is_max_count_number_less = count > max_count_number;
-            match (&props.color, &props.dot, &props.status) {
-                (None, None, None) => {
-                    if is_max_count_number_less {
-                        html! {
-                          <sup
-                            data-show="true"
-                            class="ant-scroll-number ant-badge-count ant-badge-multiple-words"
-                            title={title}
-                            style={offset_style}
-                          >
-                            {max_count_number_text}
-                          </sup>
-                        }
-                    } else {
-                        html! {
-                          <>
-                            if should_render {
-                              <sup
-                                data-show="true"
-                                class="ant-scroll-number ant-badge-count"
-                                title={title}
-                                style={offset_style}
-                              >
-                                {for numbers}
-                              </sup>
-                            }
-                          </>
-                        }
-                    }
-                }
-                (None, None, Some(_)) => {
-                    html! { <sup data-show="true" class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)} style={offset_style} title={title}></sup> }
-                }
-                (None, Some(_), None) => {
-                    html! { <sup data-show="true" class="ant-scroll-number ant-badge-dot" style={offset_style} title={title}></sup> }
-                }
-                (None, Some(_), Some(_)) => {
-                    html! { <sup data-show="true" class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)} style={offset_style} title={title}></sup> }
-                }
-                (Some(color), None, None) => {
-                    if count == 0 && props.show_zero.is_none() {
-                        html! {}
-                    } else if is_max_count_number_less {
-                        html! {
-                          <sup
-                            data-show="true"
-                            class="ant-scroll-number ant-badge-count ant-badge-multiple-words"
-                            title={title}
-                            style={format!("background: {}; {}", color.to_css_string(), offset_style)}
-                          >
-                            {max_count_number_text}
-                          </sup>
-                        }
-                    } else {
-                        html! {
-                          <>
-                            if should_render {
-                              <sup
-                                data-show="true"
-                                class="ant-scroll-number ant-badge-count"
-                                title={title}
-                                style={format!("background: {}; {}", color.to_css_string(), offset_style)}
-                              >
-                                {for numbers}
-                              </sup>
-                            }
-                          </>
-                        }
-                    }
-                }
-                (Some(color), None, Some(_)) => html! {
-                   <sup
-                     data-show="true"
-                     class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)}
-                     style={format!("background: {}; {}", color.to_css_string(), offset_style)}
-                     title={title}
-                   >
-                   </sup>
-                },
-                (Some(color), Some(_), None) => html! {
-                    <sup data-show="true" class="ant-scroll-number ant-badge-dot" style={format!("background: {}; {}", color.to_css_string(), offset_style)} title={title}></sup>
-                },
-                (Some(color), Some(_), Some(_)) => html! {
-                    <sup data-show="true" class={format!("ant-scroll-number ant-badge-dot {}", status_dot_class)} style={format!("background: {}; {}", color.to_css_string(), offset_style)} title={title}></sup>
-                },
-            }
-        }
     };
     html! {
-      <span class={format!("ant-badge {}", status_class)}>
+      <span class={span_class}>
         { for props.children.iter() }
         {sup}
-        {text_html}
+        // {text_html}
       </span>
     }
 }
