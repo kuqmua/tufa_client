bb0a76c5e79ff78e02b09f35ec022acf6dc07290
kuqmua <serezhashmakov@outlook.com>
2022-07-17 09:39:02 +0300

    pub fn get_uuid() -> UUIDStruct

--- src/components/rc/rc_progress/hooks/use_id.rs
+++ src/components/rc/rc_progress/hooks/use_id.rs
@@ -1,10 +1,41 @@
+use lazy_static::lazy_static;
+use crate::components::rc::rc_animate::util::motion::can_use_dom;
+use std::sync::Mutex;
+
 // import * as React from 'react';
 // import canUseDom from 'rc-util/lib/Dom/canUseDom';
 
+lazy_static! {
+    pub static ref UUID: Mutex<i32> = Mutex::new(0);
+}
+
+// pub const UUID: i32 = 0;
 // let uuid = 0;
+
+lazy_static! {
+    pub static ref IS_BROWSER_CLIENT: bool = can_use_dom();
+}
 
 // /** Is client side and not jsdom */
 // export const isBrowserClient = process.env.NODE_ENV !== 'test' && canUseDom();
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum UUIDStruct {
+    Number(i32),
+    String(String),
+}
+
+pub fn get_uuid() -> UUIDStruct {
+  let ret_id: UUIDStruct;
+  if *IS_BROWSER_CLIENT {
+    ret_id = UUIDStruct::Number(*UUID.lock().unwrap());
+    *UUID.lock().unwrap() += 1;
+  }
+  else {
+    ret_id = UUIDStruct::String(String::from("TEST_OR_SSR'"));
+  }
+  ret_id
+}
 
 // /** Get unique id for accessibility usage */
 // function getUUID(): number | string {
