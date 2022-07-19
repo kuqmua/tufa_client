f22920bf41ff465204d20dbd218e425e2643ec91
kuqmua <serezhashmakov@outlook.com>
2022-07-19 21:28:51 +0300

    lil fixes

--- src/components/rc/rc_progress/circle.rs
+++ src/components/rc/rc_progress/circle.rs
@@ -68,12 +68,12 @@ impl CircleStyle {
         format!(
             "
           stroke: {};
-          stroke_dash_array: {};
-          stroke_dash_offset: {};
+          stroke-dasharray: {};
+          stroke-dashoffset: {};
           transform: {};
-          transform_origin: {};
+          transform-origin: {};
           transition: {};
-          fill_opacity: {};
+          fill-opacity: {};
         ",
             self.stroke.clone().unwrap_or(String::from("")),
             self.stroke_dash_array,
@@ -353,8 +353,8 @@ pub fn circle(props: &ProgressProps) -> Html {
               cx={(VIEW_BOX_SIZE / 2.0).to_string()}
               cy={(VIEW_BOX_SIZE / 2.0).to_string()}
               stroke={stroke}
-              stroke_linecap={props.stroke_linecap.clone().unwrap_or(StrokeLinecapType::Round).get_value()}
-              stroke_width={props.stroke_width.clone().unwrap_or(1.0).to_string()}
+              stroke-linecap={props.stroke_linecap.clone().unwrap_or(StrokeLinecapType::Round).get_value()}
+              stroke-width={props.stroke_width.clone().unwrap_or(1.0).to_string()}
               opacity={opacity}
               style={circle_style_for_stack.to_string()}
               // ref={(elem) => {
@@ -417,21 +417,21 @@ pub fn circle(props: &ProgressProps) -> Html {
                     * 100.0)
                     / perimeter_without_gap;
                 html! {
-                                            <circle
-                          key={index}
-                          class={format!("{}-circle-path", props.prefix_cls.clone().unwrap_or(String::from("rc-progress")))}
-                          r={radius.to_string()}
-                          cx={(VIEW_BOX_SIZE / 2.0).to_string()}
-                          cy={(VIEW_BOX_SIZE / 2.0).to_string()}
-                          stroke={stroke}
-                          // strokeLinecap={strokeLinecap}
-                          stroke_width={props.stroke_width.clone().unwrap_or(1.0).to_string()}
-                          opacity={1}
-                          style={circle_style_for_stack.to_string()}
-                        //   ref={(elem) => {
-                        //     paths[index] = elem;
-                        //   }}
-                        />
+                    <circle
+                      key={index}
+                      class={format!("{}-circle-path", props.prefix_cls.clone().unwrap_or(String::from("rc-progress")))}
+                      r={radius.to_string()}
+                      cx={(VIEW_BOX_SIZE / 2.0).to_string()}
+                      cy={(VIEW_BOX_SIZE / 2.0).to_string()}
+                      stroke={stroke}
+                      // strokeLinecap={strokeLinecap}
+                      stroke-width={props.stroke_width.clone().unwrap_or(1.0).to_string()}
+                      opacity={1}
+                      style={circle_style_for_stack.to_string()}
+                      //   ref={(elem) => {
+                      //     paths[index] = elem;
+                      //   }}
+                    />
                 }
             })
             .collect::<Vec<Html>>();
@@ -488,7 +488,7 @@ pub fn circle(props: &ProgressProps) -> Html {
     html! {
         <svg
           class={format!("{}-circle {}", props.prefix_cls.clone().unwrap_or(String::from("rc-progress")), props.class_name.clone().unwrap_or(String::from("")))}//classNames(`${prefixCls}-circle`, className)
-          view_box={format!("0 0 {} {}", VIEW_BOX_SIZE, VIEW_BOX_SIZE)}
+          viewBox={format!("0 0 {} {}", VIEW_BOX_SIZE, VIEW_BOX_SIZE)}
           style={props.style.clone()}
           id={props.id.clone()}
           transition={props.transition.clone()}
@@ -513,8 +513,8 @@ pub fn circle(props: &ProgressProps) -> Html {
               cx={(VIEW_BOX_SIZE / 2.0).to_string()}
               cy={(VIEW_BOX_SIZE / 2.0).to_string()}
               stroke={props.trail_color.clone().unwrap_or(String::from("#D9D9D9"))}
-              stroke_linecap={props.stroke_linecap.clone().unwrap_or(StrokeLinecapType::Round).get_value()}
-              stroke_width={stroke_width.to_string()}
+              stroke-linecap={props.stroke_linecap.clone().unwrap_or(StrokeLinecapType::Round).get_value()}
+              stroke-width={stroke_width.to_string()}
               style={circle_style.to_string()}
             />
             {stroke_list}
@@ -538,179 +538,4 @@ pub fn circle(props: &ProgressProps) -> Html {
         </svg>
     }
 }
-
-// const Circle: React.FC<ProgressProps> = ({
-//   id,
-//   prefixCls,
-//   steps,
-//   strokeWidth,
-//   trailWidth,
-//   gapDegree = 0,
-//   gapPosition,
-//   trailColor,
-//   strokeLinecap,
-//   style,
-//   className,
-//   strokeColor,
-//   percent,
-//   ...restProps
-// }) => {
-//   const mergedId = useId(id);
-//   const gradientId = `${mergedId}-gradient`;
-//   const radius = VIEW_BOX_SIZE / 2 - strokeWidth / 2;
-//   const perimeter = Math.PI * 2 * radius;
-//   const rotateDeg = gapDegree > 0 ? 90 + gapDegree / 2 : -90;
-//   const perimeterWithoutGap = perimeter * ((360 - gapDegree) / 360);
-//   const { count: stepCount, space: stepSpace } =
-//     typeof steps === 'object' ? steps : { count: steps, space: 2 };
-
-//   const circleStyle = getCircleStyle(
-//     perimeter,
-//     perimeterWithoutGap,
-//     0,
-//     100,
-//     rotateDeg,
-//     gapDegree,
-//     gapPosition,
-//     trailColor,
-//     strokeLinecap,
-//     strokeWidth,
-//   );
-//   const percentList = toArray(percent);
-//   const strokeColorList = toArray(strokeColor);
-//   const gradient = strokeColorList.find((color) => color && typeof color === 'object');
-
-//   const paths = useTransitionDuration();
-
-//   const getStokeList = () => {
-//     let stackPtg = 0;
-//     return percentList
-//       .map((ptg, index) => {
-//         const color = strokeColorList[index] || strokeColorList[strokeColorList.length - 1];
-//         const stroke = color && typeof color === 'object' ? `url(#${gradientId})` : undefined;
-//         const circleStyleForStack = getCircleStyle(
-//           perimeter,
-//           perimeterWithoutGap,
-//           stackPtg,
-//           ptg,
-//           rotateDeg,
-//           gapDegree,
-//           gapPosition,
-//           color,
-//           strokeLinecap,
-//           strokeWidth,
-//         );
-//         stackPtg += ptg;
-//         return (
-//           <circle
-//             key={index}
-//             className={`${prefixCls}-circle-path`}
-//             r={radius}
-//             cx={VIEW_BOX_SIZE / 2}
-//             cy={VIEW_BOX_SIZE / 2}
-//             stroke={stroke}
-//             strokeLinecap={strokeLinecap}
-//             strokeWidth={strokeWidth}
-//             opacity={ptg === 0 ? 0 : 1}
-//             style={circleStyleForStack}
-//             ref={(elem) => {
-//               // https://reactjs.org/docs/refs-and-the-dom.html#callback-refs
-//               // React will call the ref callback with the DOM element when the component mounts,
-//               // and call it with `null` when it unmounts.
-//               // Refs are guaranteed to be up-to-date before componentDidMount or componentDidUpdate fires.
-
-//               paths[index] = elem;
-//             }}
-//           />
-//         );
-//       })
-//       .reverse();
-//   };
-
-//   const getStepStokeList = () => {
-//     // only show the first percent when pass steps
-//     const current = Math.round(stepCount * (percentList[0] / 100));
-//     const stepPtg = 100 / stepCount;
-
-//     let stackPtg = 0;
-//     return new Array(stepCount).fill(null).map((_, index) => {
-//       const color = index <= current - 1 ? strokeColorList[0] : trailColor;
-//       const stroke = color && typeof color === 'object' ? `url(#${gradientId})` : undefined;
-//       const circleStyleForStack = getCircleStyle(
-//         perimeter,
-//         perimeterWithoutGap,
-//         stackPtg,
-//         stepPtg,
-//         rotateDeg,
-//         gapDegree,
-//         gapPosition,
-//         color,
-//         'butt',
-//         strokeWidth,
-//         stepSpace,
-//       );
-//       stackPtg +=
-//         ((perimeterWithoutGap - circleStyleForStack.strokeDashoffset + stepSpace) * 100) /
-//         perimeterWithoutGap;
-
-//       return (
-//         <circle
-//           key={index}
-//           className={`${prefixCls}-circle-path`}
-//           r={radius}
-//           cx={VIEW_BOX_SIZE / 2}
-//           cy={VIEW_BOX_SIZE / 2}
-//           stroke={stroke}
-//           // strokeLinecap={strokeLinecap}
-//           strokeWidth={strokeWidth}
-//           opacity={1}
-//           style={circleStyleForStack}
-//           ref={(elem) => {
-//             paths[index] = elem;
-//           }}
-//         />
-//       );
-//     });
-//   };
-
-//   return (
-//     <svg
-//       className={classNames(`${prefixCls}-circle`, className)}
-//       viewBox={`0 0 ${VIEW_BOX_SIZE} ${VIEW_BOX_SIZE}`}
-//       style={style}
-//       id={id}
-//       {...restProps}
-//     >
-//       {gradient && (
-//         <defs>
-//           <linearGradient id={gradientId} x1="100%" y1="0%" x2="0%" y2="0%">
-//             {Object.keys(gradient)
-//               .sort((a, b) => stripPercentToNumber(a) - stripPercentToNumber(b))
-//               .map((key, index) => (
-//                 <stop key={index} offset={key} stopColor={gradient[key]} />
-//               ))}
-//           </linearGradient>
-//         </defs>
-//       )}
-//       {!stepCount && (
-//         <circle
-//           className={`${prefixCls}-circle-trail`}
-//           r={radius}
-//           cx={VIEW_BOX_SIZE / 2}
-//           cy={VIEW_BOX_SIZE / 2}
-//           stroke={trailColor}
-//           strokeLinecap={strokeLinecap}
-//           strokeWidth={trailWidth || strokeWidth}
-//           style={circleStyle}
-//         />
-//       )}
-//       {stepCount ? getStepStokeList() : getStokeList()}
-//     </svg>
-//   );
-// };
-
-// Circle.defaultProps = defaultProps;
-
 // Circle.displayName = 'Circle';
-
-// export default Circle;
