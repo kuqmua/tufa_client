492905155a04956c9848902b9ceefa761bb25bd1
kuqmua <serezhashmakov@outlook.com>
2022-07-29 21:27:38 +0300

    #[function_component(TransBtn)] partially done

--- src/components/rc/rc_select/trans_btn.rs
+++ src/components/rc/rc_select/trans_btn.rs
@@ -1,4 +1,6 @@
 use web_sys::MouseEvent;
+use yew::function_component;
+use yew::html;
 use yew::Callback;
 use yew::Children;
 use yew::Html;
@@ -11,7 +13,7 @@ use yew::Properties;
 #[derive(Debug, PartialEq, Properties, Clone)]
 pub struct TransBtnProps {
     pub class_name: String,
-    pub customize_icon: Html,
+    pub customize_icon: Option<Html>,
     //   pub customize_icon_props: Option<any>,
     pub on_mouse_down: Option<Callback<MouseEvent>>, //React.MouseEventHandler<HTMLSpanElement>
     pub on_click: Option<Callback<MouseEvent>>,      //React.MouseEventHandler<HTMLSpanElement>
@@ -26,6 +28,46 @@ pub struct TransBtnProps {
 //   onClick?: React.MouseEventHandler<HTMLSpanElement>;
 //   children?: React.ReactNode;
 // }
+
+#[function_component(TransBtn)]
+pub fn trans_btn(props: &TransBtnProps) -> Html {
+    let on_click = match props.on_click.clone() {
+        None => Callback::from(|_: MouseEvent| {}),
+        Some(okd) => okd,
+    };
+    let on_mouse_down = match props.on_mouse_down.clone() {
+        None => Callback::from(|e: MouseEvent| {
+            e.prevent_default();
+        }),
+        Some(okd) => Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            okd.emit(e);
+        }),
+    };
+    let content = match props.customize_icon.clone() {
+        None => {
+            // let b = classNames(className.split(/\s+/).map((cls) => `${cls}-icon`));
+            html! {
+              <span class={String::from("")}>
+                {props.children.clone()}
+              </span>
+            }
+        }
+        Some(ci) => ci,
+    };
+    html! {
+      <span
+        class={props.class_name.clone()}
+        onmousedown={on_mouse_down.clone()}
+        style={"user-select: none; -webkit-user-select: none".to_string()}
+        unselectable="on"
+        onclick={on_click}
+        aria-hidden={"true"}
+      >
+        {content}
+      </span>
+    }
+}
 
 // const TransBtn: React.FC<TransBtnProps> = ({
 //   className,
