3d2c1f08c1612472f7c66db0e7e6108ef1f4ceb3
kuqmua <serezhashmakov@outlook.com>
2022-08-04 18:49:27 +0300

    lil tweaks

--- src/components/rc/rc_select/selector/mod.rs
+++ src/components/rc/rc_select/selector/mod.rs
@@ -98,7 +98,7 @@ pub struct RefSelectorProps {
 //   scrollTo?: ScrollTo;
 // }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum MaxTagCount {
     Number(i32),
     Responsive,
@@ -190,6 +190,196 @@ pub struct SelectorProps {
 //    */
 //   domRef: React.Ref<HTMLDivElement>;
 // }
+
+#[derive(Debug, Properties, PartialEq, Clone)]
+pub struct SelectorPropsWithRefSelectorProps {
+    pub id: String,
+    pub prefix_cls: String,
+    pub show_search: Option<()>,
+    pub open: Option<()>,
+    /** Display in the Selector value, it's not same as `value` prop */
+    // pub values: DisplayValueType[],
+    // pub mode: Mode,
+    pub search_value: String,
+    pub active_value: String,
+    pub input_element: Html,
+    pub max_length: Option<i32>,
+
+    pub auto_focus: Option<()>,
+    pub active_descendant_id: Option<String>,
+    pub tab_index: Option<i32>,
+    pub disabled: Option<()>,
+    pub placeholder: Option<Html>,
+    pub remove_icon: Option<Html>,
+
+    pub max_tag_count: Option<MaxTagCount>,
+    pub max_tag_text_length: Option<i32>,
+    pub max_tag_placeholder: Option<Html>,
+    pub tag_render: Option<Callback<Html>>,
+
+    /** Check if `tokenSeparators` contains `\n` or `\r\n` */
+    pub token_with_enter: Option<()>,
+
+    // Motion
+    pub choice_transition_name: Option<String>,
+
+    pub on_toggle_open: Option<Callback<()>>,
+    /** `onSearch` returns go next step boolean to check if need do toggle open */
+    pub on_search: Callback<bool>,
+    pub on_search_submit: Option<Callback<()>>,
+    pub on_remove: Callback<()>,
+    pub on_input_key_down: Option<Callback<KeyboardEvent>>,
+    pub dom_ref: NodeRef,
+
+    //
+    pub focus: Callback<()>,
+    pub blur: Callback<()>,
+    // pub scroll_to?: ScrollTo;
+}
+
+#[function_component(Selector)]
+pub fn selector(props: &SelectorPropsWithRefSelectorProps) -> Html {
+    let on_input_key_down = props.on_input_key_down.clone();
+    let on_internal_input_key_down = Callback::from(move |event: KeyboardEvent| {
+        let key = event.key();
+        let up = String::from("UP");
+        let down = String::from("DOWN");
+        if key == up {
+            event.prevent_default();
+        }
+        if key == down {
+            event.prevent_default();
+        }
+        if on_input_key_down.clone().is_some() {
+            event.prevent_default();
+        }
+        //     if (which === KeyCode.ENTER && mode === 'tags' && !compositionStatusRef.current && !open) {
+        //       // When menu isn't open, OptionList won't trigger a value change
+        //       // So when enter is pressed, the tag's input value should be emitted here to let selector know
+        //       onSearchSubmit?.((event.target as HTMLInputElement).value);
+        //     }
+
+        //     if (isValidateOpenKey(which)) {
+        //       onToggleOpen(true);
+        //     }
+        //   };
+
+        //   /**
+        //    * We can not use `findDOMNode` sine it will get warning,
+        //    * have to use timer to check if is input element.
+        //    */
+        //   const onInternalInputMouseDown: React.MouseEventHandler<HTMLInputElement> = () => {
+        //     setInputMouseDown(true);
+        //   };
+
+        //   // When paste come, ignore next onChange
+        //   const pastedTextRef = useRef<string>(null);
+
+        //   const triggerOnSearch = (value: string) => {
+        //     if (onSearch(value, true, compositionStatusRef.current) !== false) {
+        //       onToggleOpen(true);
+        //     }
+        //   };
+
+        //   const onInputCompositionStart = () => {
+        //     compositionStatusRef.current = true;
+        //   };
+
+        //   const onInputCompositionEnd: React.CompositionEventHandler<HTMLInputElement> = (e) => {
+        //     compositionStatusRef.current = false;
+
+        //     // Trigger search again to support `tokenSeparators` with typewriting
+        //     if (mode !== 'combobox') {
+        //       triggerOnSearch((e.target as HTMLInputElement).value);
+        //     }
+        //   };
+
+        //   const onInputChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+        //     let {
+        //       target: { value },
+        //     } = event;
+
+        //     // Pasted text should replace back to origin content
+        //     if (tokenWithEnter && pastedTextRef.current && /[\r\n]/.test(pastedTextRef.current)) {
+        //       // CRLF will be treated as a single space for input element
+        //       const replacedText = pastedTextRef.current
+        //         .replace(/[\r\n]+$/, '')
+        //         .replace(/\r\n/g, ' ')
+        //         .replace(/[\r\n]/g, ' ');
+        //       value = value.replace(replacedText, pastedTextRef.current);
+        //     }
+
+        //     pastedTextRef.current = null;
+
+        //     triggerOnSearch(value);
+        //   };
+
+        //   const onInputPaste: React.ClipboardEventHandler = (e) => {
+        //     const { clipboardData } = e;
+        //     const value = clipboardData.getData('text');
+
+        //     pastedTextRef.current = value;
+        //   };
+
+        //   const onClick = ({ target }) => {
+        //     if (target !== inputRef.current) {
+        //       // Should focus input if click the selector
+        //       const isIE = (document.body.style as any).msTouchAction !== undefined;
+        //       if (isIE) {
+        //         setTimeout(() => {
+        //           inputRef.current.focus();
+        //         });
+        //       } else {
+        //         inputRef.current.focus();
+        //       }
+        //     }
+        //   };
+
+        //   const onMouseDown: React.MouseEventHandler<HTMLElement> = (event) => {
+        //     const inputMouseDown = getInputMouseDown();
+        //     if (event.target !== inputRef.current && !inputMouseDown) {
+        //       event.preventDefault();
+        //     }
+
+        //     if ((mode !== 'combobox' && (!showSearch || !inputMouseDown)) || !open) {
+        //       if (open) {
+        //         onSearch('', true, false);
+        //       }
+        //       onToggleOpen();
+        //     }
+        //   };
+
+        //   // ================= Inner Selector ==================
+        //   const sharedProps = {
+        //     inputRef,
+        //     onInputKeyDown: onInternalInputKeyDown,
+        //     onInputMouseDown: onInternalInputMouseDown,
+        //     onInputChange,
+        //     onInputPaste,
+        //     onInputCompositionStart,
+        //     onInputCompositionEnd,
+        //   };
+
+        //   const selectNode =
+        //     mode === 'multiple' || mode === 'tags' ? (
+        //       <MultipleSelector {...props} {...sharedProps} />
+        //     ) : (
+        //       <SingleSelector {...props} {...sharedProps} />
+        //     );
+
+        //   return (
+        //     <div
+        //       ref={domRef}
+        //       className={`${prefixCls}-selector`}
+        //       onClick={onClick}
+        //       onMouseDown={onMouseDown}
+        //     >
+        //       {selectNode}
+        //     </div>
+        //   );
+    });
+    html! {}
+}
 
 // const Selector: React.RefForwardingComponent<RefSelectorProps, SelectorProps> = (props, ref) => {
 //   const inputRef = useRef<HTMLInputElement>(null);
