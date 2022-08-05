8ffd2e66d6ae6d48e16ebb9765b0bb63c540de92
Sergey Shmakov <31436321+kuqmua@users.noreply.github.com>
2022-08-05 16:02:44 +0300

    Update select.rs

--- src/components/ant_design/data_entry/select.rs
+++ src/components/ant_design/data_entry/select.rs
@@ -8,6 +8,7 @@ use yew::Callback;
 use yew::Html;
 use yew::Properties;
 use yew::UseStateHandle;
+use web_sys::window;
 
 #[derive(Debug, PartialEq, Properties, Clone)]
 pub struct SelectProps {
@@ -15,7 +16,8 @@ pub struct SelectProps {
     pub default_value: String,
     pub style: Option<String>,
     pub additional_classes: Option<String>,
-    pub set_choosen_value: Callback<(Event, String)>,
+    pub set_choosen_value: Callback<(Event, Option<String>)>,
+    pub id: String, 
 }
 
 #[function_component(Select)]
@@ -25,16 +27,35 @@ pub fn select(props: &SelectProps) -> Html {
     let choosen_value_cloned = choosen_value.clone();
     let choosen_value_second_cloned = choosen_value.clone();
     let set_choosen_value_cloned = props.set_choosen_value.clone();
+    let genereted_id = format!("select-{}", props.id.clone());
+    let genereted_id_cloned = genereted_id.clone();
     let on_open = Callback::<Event>::from(move |e: Event| {
         // e.prevent_default();
         log!("wtf");
         is_open.set(!*is_open.clone());
-        let value = &*choosen_value_second_cloned.clone();
-        set_choosen_value_cloned.emit((e, value.to_string()));
+//         let value = &*choosen_value_second_cloned.clone();
+        //
+        //or maybe https://docs.rs/yew/0.9.1/yew/components/select/index.html
+        //HtmlSelectElement
+        //selected_index
+        //https://rustwasm.github.io/wasm-bindgen/api/web_sys/struct.HtmlSelectElement.html#method.selected_index
+        let value: Option<String> = match window() {
+          None => None,
+          Some(window) => match window.document() {
+            None => None,
+            Some(document) => match document.get_element_by_id(genereted_id_cloned) {
+                //something to test creation dom method, no actual need in created element
+                None => None,
+                Some(element) => Some(element.id),
+            },
+          },
+        }
+        //
+        set_choosen_value_cloned.emit((e, value));
     });
-    let on_click = Callback::<MouseEvent>::from(move |e: MouseEvent| {
-        log!("oooo");
-    });
+//     let on_click = Callback::<MouseEvent>::from(move |e: MouseEvent| {
+//         log!("oooo");
+//     });
     let options = props
         .values
         .iter()
@@ -42,11 +63,11 @@ pub fn select(props: &SelectProps) -> Html {
         .map(|(index, v)| {
             if v.clone() == *choosen_value_cloned.clone() {
                 html! {
-                    <option selected={true} value={index.to_string()}>{v}</option>
+                    <option id={format!("{}{}", )} selected={true} value={index.to_string()}>{v}</option>
                 }
             } else {
                 html! {
-                    <option value={index.to_string()}>{v}</option>
+                    <option id={} value={index.to_string()}>{v}</option>
                 }
             }
         })
@@ -61,8 +82,9 @@ pub fn select(props: &SelectProps) -> Html {
     };
     html! {
       <select
-        onclick={on_click}
-        onchange={on_open.clone()}
+        id={}
+        onclick={on_open.clone()}
+//         onchange={on_open.clone()}
         class={classes}
         style={style}
       >
