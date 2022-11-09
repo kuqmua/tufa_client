0d205c5b980682a06c1e364b9c8ac393ac360721
kuqmua <serezhashmakov@outlook.com>
2022-11-09 20:35:51 +0300

    GIT_INFO without .data field init

--- src/once_cell_globals/git_info.rs
+++ src/once_cell_globals/git_info.rs
@@ -1,6 +1,6 @@
 use crate::constants::PROJECT_NAME;
 use once_cell::sync::Lazy;
-use tufa_common::common::git::git_info_wrapper::GitInformationWrapper;
+use tufa_common::common::git::git_info::GitInformation;
 
-pub static GIT_INFO: Lazy<GitInformationWrapper> =
-    Lazy::new(|| GitInformationWrapper::init("../", PROJECT_NAME));
+pub static GIT_INFO: Lazy<GitInformation> =
+    Lazy::new(|| GitInformation::get_git_commit_info("../", PROJECT_NAME));
