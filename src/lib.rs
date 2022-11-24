4f8d0fa57e583c854dd1531d3b31973e753c1ea9
kuqmua <serezhashmakov@outlook.com>
2022-11-24 21:28:53 +0300

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
+#![deny(
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::unwrap_used,
+    clippy::float_arithmetic
+)]
 #![allow(clippy::too_many_arguments)]
+
 pub mod components;
 pub mod entry;
 pub mod global_variables;
