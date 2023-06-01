b49bd1035bb59e59029e47e93ed78603179cd84e
kuqmua <serezhashmakov@outlook.com>
2023-06-02 01:27:31 +0300

    lil git_info usage tweaks

--- src/global_variables/compile_time/git_info.rs
+++ src/global_variables/compile_time/git_info.rs
@@ -1,2 +1,2 @@
 pub static GIT_INFO: tufa_common::common::git::git_info::GitInformation =
-    compile_time_git_info::compile_time_git_info!(tufa_client, tufa_common);
+    compile_time_git_info::compile_time_git_info!(tufa_client);
