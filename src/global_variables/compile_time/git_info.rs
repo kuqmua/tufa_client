d3e54109cca959738db659f2ba666eb41a336fa0
kuqmua <serezhashmakov@outlook.com>
2023-09-06 20:17:03 +0300

    static -> const

--- src/global_variables/compile_time/git_info.rs
+++ src/global_variables/compile_time/git_info.rs
@@ -1,2 +1,2 @@
-pub static GIT_INFO: tufa_common::common::git::git_info::GitInfo =
+pub const GIT_INFO: tufa_common::common::git::git_info::GitInfo =
     compile_time_git_info::compile_time_git_info!(tufa_client);
